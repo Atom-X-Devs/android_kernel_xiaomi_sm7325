543a1f72aa9d1bdda48ceb21797ac8efa2d12394
Lina Iyer <ilina@codeaurora.org>
2014-05-21 16:21:31 -0600

    ANDROID: GKI: QoS: Enhance framework to support cpu/irq specific QoS requests
    QoS request for CPU_DMA_LATENCY can be better optimized if the request
    can be set only for the required cpus and not all cpus. This helps save
    power on other cores, while still gauranteeing the quality of service.
    
    Enhance the QoS constraints data structures to support target value for
    each core. Requests specify if the QoS is applicable to all cores
    (default) or to a selective subset of the cores or to a core(s), that the
    IRQ is affine to.
    
    QoS requests that need to track an IRQ can be set to apply only on the
    cpus to which the IRQ's smp_affinity attribute is set to. The QoS
    framework will automatically track IRQ migration between the cores. The
    QoS is updated to be applied only to the core(s) that the IRQ has been
    migrated to.
    
    Idle and interested drivers can request a PM QoS value for a constraint
    across all cpus, or a specific cpu or a set of cpus. Separate APIs have
    been added to request for individual cpu or a cpumask.  The default
    behaviour of PM QoS is maintained i.e, requests that do not specify a
    type of the request will continue to be effected on all cores.  Requests
    that want to specify an affinity of cpu(s) or an irq, can modify the PM
    QoS request data structures by specifying the type of the request and
    either the mask of the cpus or the IRQ number depending on the type.
    Updating the request does not reset the type of the request.
    
    The userspace sysfs interface does not support CPU/IRQ affinity.
    
     Divyanshu-Modi: Adapt to latest qos changes
    
    Signed-off-by: Lina Iyer <ilina@codeaurora.org>
    (cherry picked from commit 16cafdb44fb3a66a7d06936d775efe483ad62b7d)
    
    Bug: 153463922
    Test: build
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: I1aad836a985fd303f2254fe607bb909a6b720dd5
    Co-authored-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- include/linux/pm_qos.h
+++ include/linux/pm_qos.h
@@ -15,6 +15,8 @@
 #include <linux/plist.h>
 #include <linux/notifier.h>
 #include <linux/device.h>
+#include <linux/cpumask.h>
+#include <linux/interrupt.h>
 
 enum pm_qos_flags_status {
 	PM_QOS_FLAGS_UNDEFINED = -1,
@@ -52,13 +54,30 @@ enum pm_qos_type {
 struct pm_qos_constraints {
 	struct plist_head list;
 	s32 target_value;	/* Do not change to 64 bit */
+	s32 target_per_cpu[NR_CPUS];
 	s32 default_value;
 	s32 no_constraint_value;
 	enum pm_qos_type type;
 	struct blocking_notifier_head *notifiers;
 };
 
+enum pm_qos_req_type {
+	PM_QOS_REQ_ALL_CORES = 0,
+	PM_QOS_REQ_AFFINE_CORES,
+#ifdef CONFIG_SMP
+	PM_QOS_REQ_AFFINE_IRQ,
+#endif
+};
+
 struct pm_qos_request {
+	enum pm_qos_req_type type;
+	struct cpumask cpus_affine;
+#ifdef CONFIG_SMP
+	uint32_t irq;
+
+	/* Internal structure members */
+	struct irq_affinity_notify irq_notify;
+#endif
 	struct plist_node node;
 	struct pm_qos_constraints *qos;
 };
@@ -142,6 +161,9 @@ int pm_qos_update_target(struct pm_qos_constraints *c, struct plist_node *node,
 bool pm_qos_update_flags(struct pm_qos_flags *pqf,
 			 struct pm_qos_flags_request *req,
 			 enum pm_qos_req_action action, s32 val);
+
+int pm_qos_request_for_cpu(int cpu);
+int pm_qos_request_for_cpumask(struct cpumask *mask);
 
 s32 cpu_latency_qos_limit(void);
 bool cpu_latency_qos_request_active(struct pm_qos_request *req);
