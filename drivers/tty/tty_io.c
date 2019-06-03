00470d0acc4e5580249f2426eed28204e7bde7a8
Arnd Bergmann <arnd@arndb.de>
2019-06-03 23:07:12 +0200

    compat_ioctl: move SIOCOUTQ out of compat_ioctl.c
    All users of this call are in socket or tty code, so handling
    it there means we can avoid the table entry in fs/compat_ioctl.c.
    
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Eric Dumazet <edumazet@google.com>
    Cc: netdev@vger.kernel.org
    Cc: "David S. Miller" <davem@davemloft.net>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -2759,6 +2759,7 @@ static long tty_compat_ioctl(struct file *file, unsigned int cmd,
 	int retval = -ENOIOCTLCMD;
 
 	switch (cmd) {
+	case TIOCOUTQ:
 	case TIOCSTI:
 	case TIOCGWINSZ:
 	case TIOCSWINSZ:
