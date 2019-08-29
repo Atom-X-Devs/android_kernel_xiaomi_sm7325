ebcbabdcf36aa1bd2870dc747d6ab4ac479798c8
Bang Nguyen <bangnguyen@motorola.com>
2019-08-29 13:28:54 -0500

    disp: msm: fix checking returned value of xx_cmd_transfer
    Change the checking returned value of dsi_ctrl_cmd_transfer(), because
    if this is a read command then it will return the number byte of
    reading when reading succeed. If this is a write command, then it
    will return "none-zero" for failure.
    
    Change-Id: I088af5da728dc758470377253b8d4e16d1222c1b
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1697593
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- msm/dsi/dsi_display.c
+++ msm/dsi/dsi_display.c
@@ -3388,7 +3388,9 @@ static ssize_t dsi_host_transfer(struct mipi_dsi_host *host,
 
 		rc = dsi_ctrl_cmd_transfer(display->ctrl[ctrl_idx].ctrl, msg,
 				&cmd_flags);
-		if (rc < 0) {
+		if (((cmd_flags & DSI_CTRL_CMD_READ) && rc <= 0) ||
+				(!(cmd_flags & DSI_CTRL_CMD_READ) && rc < 0 )) {
+
 			DSI_ERR("[%s] cmd transfer failed, rc=%d\n",
 			       display->name, rc);
 			goto error_disable_cmd_engine;
