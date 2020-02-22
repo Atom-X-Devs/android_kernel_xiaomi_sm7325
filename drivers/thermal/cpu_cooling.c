08104695699809a3e99b1484e44fe83ecd230ac9
Thara Gopinath <thara.gopinath@linaro.org>
2020-02-21 19:52:12 -0500

    thermal/cpu-cooling: Update thermal pressure in case of a maximum frequency capping
    Thermal governors can request for a CPU's maximum supported frequency to
    be capped in case of an overheat event. This in turn means that the
    maximum capacity available for tasks to run on the particular CPU is
    reduced. Delta between the original maximum capacity and capped maximum
    capacity is known as thermal pressure. Enable cpufreq cooling device to
    update the thermal pressure in event of a capped maximum frequency.
    
    Change-Id: Ic0b76751cc13205c1ed2f1b3df507fced8f8acd6
    Signed-off-by: Thara Gopinath <thara.gopinath@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lkml.kernel.org/r/20200222005213.3873-9-thara.gopinath@linaro.org
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/thermal/cpu_cooling.c
+++ drivers/thermal/cpu_cooling.c
@@ -430,6 +430,9 @@ static int cpufreq_set_cur_state(struct thermal_cooling_device *cdev,
 				 unsigned long state)
 {
 	struct cpufreq_cooling_device *cpufreq_cdev = cdev->devdata;
+	struct cpumask *cpus;
+	unsigned int frequency;
+	unsigned long max_capacity, capacity;
 	int ret;
 
 	/* Request state should be less than max_level */
@@ -440,10 +443,18 @@ static int cpufreq_set_cur_state(struct thermal_cooling_device *cdev,
 	if (cpufreq_cdev->cpufreq_state == state)
 		return 0;
 
-	ret = freq_qos_update_request(&cpufreq_cdev->qos_req,
-				get_state_freq(cpufreq_cdev, state));
-	if (ret > 0)
+	frequency = get_state_freq(cpufreq_cdev, state);
+
+	ret = freq_qos_update_request(&cpufreq_cdev->qos_req, frequency);
+
+	if (ret > 0) {
 		cpufreq_cdev->cpufreq_state = state;
+		cpus = cpufreq_cdev->policy->cpus;
+		max_capacity = arch_scale_cpu_capacity(cpumask_first(cpus));
+		capacity = frequency * max_capacity;
+		capacity /= cpufreq_cdev->policy->cpuinfo.max_freq;
+		arch_set_thermal_pressure(cpus, max_capacity - capacity);
+	}
 
 	return ret;
 }
