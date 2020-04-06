ce4c0c04544f5762a03997177803788082f9d866
Quentin Perret <qperret@google.com>
2020-04-06 16:40:02 +0100

    Revert "ANDROID: arm: enable max frequency capping"
    This reverts commit 150b7ce48a29566d996bda293e8857ef45298bdc.
    
    Upstream has an equivalent feature now, drop the Android-specific
    version.
    
    Bug: 120440300
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: I2298a79b9ca1f5798643c3ab09b2ee63630e0e89
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- arch/arm/include/asm/topology.h
+++ arch/arm/include/asm/topology.h
@@ -12,9 +12,6 @@
 #define arch_scale_freq_capacity topology_get_freq_scale
 #define arch_scale_freq_invariant topology_scale_freq_invariant
 
-/* Replace task scheduler's default max-frequency-invariant accounting */
-#define arch_scale_max_freq_capacity topology_get_max_freq_scale
-
 /* Replace task scheduler's default cpu-invariant accounting */
 #define arch_scale_cpu_capacity topology_get_cpu_scale
 
