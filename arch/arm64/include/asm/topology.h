6fa1e47386009265b94816d869d787d23d6dd532
Valentin Schneider <valentin.schneider@arm.com>
2020-07-12 17:59:15 +0100

    arch_topology, sched/core: Cleanup thermal pressure definition
    The following commit:
    
      14533a16c46d ("thermal/cpu-cooling, sched/core: Move the arch_set_thermal_pressure() API to generic scheduler code")
    
    moved the definition of arch_set_thermal_pressure() to sched/core.c, but
    kept its declaration in linux/arch_topology.h. When building e.g. an x86
    kernel with CONFIG_SCHED_THERMAL_PRESSURE=y, cpufreq_cooling.c ends up
    getting the declaration of arch_set_thermal_pressure() from
    include/linux/arch_topology.h, which is somewhat awkward.
    
    On top of this, sched/core.c unconditionally defines
    o The thermal_pressure percpu variable
    o arch_set_thermal_pressure()
    
    while arch_scale_thermal_pressure() does nothing unless redefined by the
    architecture.
    
    arch_*() functions are meant to be defined by architectures, so revert the
    aforementioned commit and re-implement it in a way that keeps
    arch_set_thermal_pressure() architecture-definable, and doesn't define the
    thermal pressure percpu variable for kernels that don't need
    it (CONFIG_SCHED_THERMAL_PRESSURE=n).
    
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200712165917.9168-2-valentin.schneider@arm.com
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Change-Id: Ibd4fb7fbf993fce961be4221a8fb01a4e4403600
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- arch/arm64/include/asm/topology.h
+++ arch/arm64/include/asm/topology.h
@@ -28,8 +28,9 @@ int pcibus_to_node(struct pci_bus *bus);
 /* Enable topology flag updates */
 #define arch_update_cpu_topology topology_update_cpu_topology
 
-/* Replace task scheduler's default thermal pressure retrieve API */
+/* Replace task scheduler's default thermal pressure API */
 #define arch_scale_thermal_pressure topology_get_thermal_pressure
+#define arch_set_thermal_pressure   topology_set_thermal_pressure
 
 #include <asm-generic/topology.h>
 
