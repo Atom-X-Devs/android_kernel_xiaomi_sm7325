5b1c6ee81b829b834e4c7a3f52f224e8757b242e
Park Bumgyu <bumgyu.park@samsung.com>
2020-08-20 11:17:23 +0900

    ANDROID: vendor_hooks: add cpuidle event
    An event that gather the idle state that the cpu attempted to enter and
    actually entered is added. Through this, the idle statistics of the cpu
    can be obtained and used for vendor specific algorithms or for system
    analysis.
    
    Bug: 162980647
    
    Change-Id: I9c2491d524722042e881864488f7b3cf7e903d1e
    Signed-off-by: Park Bumgyu <bumgyu.park@samsung.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -23,6 +23,7 @@
 #include <linux/suspend.h>
 #include <linux/tick.h>
 #include <trace/events/power.h>
+#include <trace/hooks/cpuidle.h>
 
 #include "cpuidle.h"
 
@@ -223,6 +224,7 @@ int __nocfi cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_drive
 	/* Take note of the planned idle state. */
 	sched_idle_set_state(target_state, index);
 
+	trace_android_vh_cpu_idle(0, index, dev->cpu);
 	trace_cpu_idle_rcuidle(index, dev->cpu);
 	time_start = ns_to_ktime(local_clock());
 
@@ -233,6 +235,7 @@ int __nocfi cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_drive
 	sched_clock_idle_wakeup_event();
 	time_end = ns_to_ktime(local_clock());
 	trace_cpu_idle_rcuidle(PWR_EVENT_EXIT, dev->cpu);
+	trace_android_vh_cpu_idle(PWR_EVENT_EXIT, entered_state, dev->cpu);
 
 	/* The cpu is no longer idle or about to enter idle. */
 	sched_idle_set_state(NULL, -1);
