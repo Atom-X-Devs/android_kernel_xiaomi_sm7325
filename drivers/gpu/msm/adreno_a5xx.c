0657b4e51d2ac6064995e7b06492ab7bc45906fd
Jordan Crouse <jcrouse@codeaurora.org>
2020-10-08 18:14:29 -0600

    msm: kgsl: Move irq_pending to target specific
    Move the irq_pending check to target specific code to get rid of an
    ADRNEO_REG definition.
    
    Change-Id: Ic0dedbad9713c34ac967f9bd29d2acdb35a6ea73
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/gpu/msm/adreno_a5xx.c
+++ drivers/gpu/msm/adreno_a5xx.c
@@ -2000,7 +2000,6 @@ static unsigned int a5xx_register_offsets[ADRENO_REG_REGISTER_MAX] = {
 	ADRENO_REG_DEFINE(ADRENO_REG_RBBM_STATUS, A5XX_RBBM_STATUS),
 	ADRENO_REG_DEFINE(ADRENO_REG_RBBM_STATUS3, A5XX_RBBM_STATUS3),
 	ADRENO_REG_DEFINE(ADRENO_REG_RBBM_INT_0_MASK, A5XX_RBBM_INT_0_MASK),
-	ADRENO_REG_DEFINE(ADRENO_REG_RBBM_INT_0_STATUS, A5XX_RBBM_INT_0_STATUS),
 	ADRENO_REG_DEFINE(ADRENO_REG_RBBM_CLOCK_CTL, A5XX_RBBM_CLOCK_CNTL),
 	ADRENO_REG_DEFINE(ADRENO_REG_RBBM_SW_RESET_CMD, A5XX_RBBM_SW_RESET_CMD),
 	ADRENO_REG_DEFINE(ADRENO_REG_VBIF_VERSION,
@@ -2381,7 +2380,11 @@ static bool a5xx_hw_isidle(struct adreno_device *adreno_dev)
 	if (status & 0xfffffffe)
 		return false;
 
-	return adreno_irq_pending(adreno_dev) ? false : true;
+	kgsl_regread(device, A5XX_RBBM_INT_0_STATUS, &status);
+
+	/* Return busy if a interrupt is pending */
+	return !((status & adreno_dev->irq_mask) ||
+		atomic_read(&adreno_dev->pending_irq_refcnt));
 }
 
 static int a5xx_clear_pending_transactions(struct adreno_device *adreno_dev)
