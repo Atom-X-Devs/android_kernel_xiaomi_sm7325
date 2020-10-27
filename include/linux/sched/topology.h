9259e640800a66ff4fbe931ba516284f44557ad1
Ionela Voinescu <ionela.voinescu@arm.com>
2020-10-27 18:07:11 +0000

    sched/topology,schedutil: Wrap sched domains rebuild
    Add the rebuild_sched_domains_energy() function to wrap the functionality
    that rebuilds the scheduling domains if any of the Energy Aware Scheduling
    (EAS) initialisation conditions change. This functionality is used when
    schedutil is added or removed or when EAS is enabled or disabled
    through the sched_energy_aware sysctl.
    
    Therefore, create a single function that is used in both these cases and
    that can be later reused.
    
    Change-Id: I0ab05338a322624d9b678425e8037a2382ffe5f7
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Quentin Perret <qperret@google.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://lkml.kernel.org/r/20201027180713.7642-2-ionela.voinescu@arm.com
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- include/linux/sched/topology.h
+++ include/linux/sched/topology.h
@@ -226,6 +226,14 @@ static inline bool cpus_share_cache(int this_cpu, int that_cpu)
 
 #endif	/* !CONFIG_SMP */
 
+#if defined(CONFIG_ENERGY_MODEL) && defined(CONFIG_CPU_FREQ_GOV_SCHEDUTIL)
+extern void rebuild_sched_domains_energy(void);
+#else
+static inline void rebuild_sched_domains_energy(void)
+{
+}
+#endif
+
 #ifndef arch_scale_cpu_capacity
 static __always_inline
 unsigned long arch_scale_cpu_capacity(int cpu)
