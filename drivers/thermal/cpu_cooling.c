54547ad749641b41310e883754d89ba2c5752ba8
Zhuguangqing <zhuguangqing@xiaomi.com>
2020-11-06 17:22:43 +0800

    thermal/drivers/cpufreq_cooling: Update cpufreq_state only if state has changed
    commit 236761f19a4f373354f1dcf399b57753f1f4b871 upstream.
    
    If state has not changed successfully and we updated cpufreq_state,
    next time when the new state is equal to cpufreq_state (not changed
    successfully last time), we will return directly and miss a
    freq_qos_update_request() that should have been.
    
    Fixes: 5130802ddbb1 ("thermal: cpu_cooling: Switch to QoS requests for freq limits")
    Cc: v5.4+ <stable@vger.kernel.org> # v5.4+
    Change-Id: I24511e042d2997f177af52de8d87105bd17909bb
    Signed-off-by: Zhuguangqing <zhuguangqing@xiaomi.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/20201106092243.15574-1-zhuguangqing83@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/thermal/cpu_cooling.c
+++ drivers/thermal/cpu_cooling.c
@@ -430,6 +430,7 @@ static int cpufreq_set_cur_state(struct thermal_cooling_device *cdev,
 				 unsigned long state)
 {
 	struct cpufreq_cooling_device *cpufreq_cdev = cdev->devdata;
+	int ret;
 
 	/* Request state should be less than max_level */
 	if (WARN_ON(state > cpufreq_cdev->max_level))
@@ -439,10 +440,12 @@ static int cpufreq_set_cur_state(struct thermal_cooling_device *cdev,
 	if (cpufreq_cdev->cpufreq_state == state)
 		return 0;
 
-	cpufreq_cdev->cpufreq_state = state;
-
-	return freq_qos_update_request(&cpufreq_cdev->qos_req,
+	ret = freq_qos_update_request(&cpufreq_cdev->qos_req,
 				get_state_freq(cpufreq_cdev, state));
+	if (ret > 0)
+		cpufreq_cdev->cpufreq_state = state;
+
+	return ret;
 }
 
 /* Bind cpufreq callbacks to thermal cooling device ops */
