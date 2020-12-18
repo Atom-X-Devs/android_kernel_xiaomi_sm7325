049b2fab3b63f44c75d51da9a9a6cebcca31e2b0
Willem de Bruijn <willemb@google.com>
2020-12-18 14:05:41 -0800

    BACKPORT: epoll: wire up syscall epoll_pwait2
    Split off from prev patch in the series that implements the syscall.
    
    Link: https://lkml.kernel.org/r/20201121144401.3727659-4-willemdebruijn.kernel@gmail.com
    Change-Id: I48dfae6f721b24ebc53de603e393289954a95908
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -854,6 +854,8 @@ __SYSCALL(__NR_clone3, sys_clone3)
 __SYSCALL(__NR_close_range, sys_close_range)
 #define __NR_process_madvise 440
 __SYSCALL(__NR_process_madvise, sys_process_madvise)
+#define __NR_epoll_pwait2 441
+__SC_COMP(__NR_epoll_pwait2, sys_epoll_pwait2, compat_sys_epoll_pwait2)
 #define __NR_process_mrelease 448
 __SYSCALL(__NR_process_mrelease, sys_process_mrelease)
 
