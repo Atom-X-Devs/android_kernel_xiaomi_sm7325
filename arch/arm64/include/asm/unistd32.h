6b0ceb604e381d7aad673f77333de0e3d407f658
Heiko Carstens <hca@linux.ibm.com>
2020-12-20 10:58:30 +0100

    BACKPORT: epoll: fix compat syscall wire up of epoll_pwait2
    Commit b0a0c2615f6f ("epoll: wire up syscall epoll_pwait2") wired up
    the 64 bit syscall instead of the compat variant in a couple of places.
    
    Fixes: b0a0c2615f6f ("epoll: wire up syscall epoll_pwait2")
    Change-Id: Ida2267e953240c036d64bc61177ee72bb20f35a3
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Willem de Bruijn <willemb@google.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Thomas Bogendoerfer <tsbogend@alpha.franken.de>
    Cc: Vasily Gorbik <gor@linux.ibm.com>
    Cc: Christian Borntraeger <borntraeger@de.ibm.com>
    Cc: "David S. Miller" <davem@davemloft.net>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -884,7 +884,7 @@ __SYSCALL(__NR_close_range, sys_close_range)
 #define __NR_process_madvise 440
 __SYSCALL(__NR_process_madvise, sys_process_madvise)
 #define __NR_epoll_pwait2 441
-__SYSCALL(__NR_epoll_pwait2, sys_epoll_pwait2)
+__SYSCALL(__NR_epoll_pwait2, compat_sys_epoll_pwait2)
 #define __NR_process_mrelease 448
 __SYSCALL(__NR_process_mrelease, sys_process_mrelease)
 
