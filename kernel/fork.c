bc85400d3a249991a65d18ae7551276bc80679f1
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-31 20:44:08 -0800

    kernel: Boost DDR bus for a short period when zygote forks
    There is a gap from when a new app is forked to the time that the
    devfreq governor managing the DDR bus can pick up the increased load and
    ramp up appropriately. Since the polling interval for this governor is
    typically 50 ms, that means it may take up to 50 ms before the governor
    ramps up, which hurts cold launch app performance. Boosting the DDR bus
    for 50 ms upon a fork from zygote covers that gap, speeding up cold app
    launches.
    
    [ghostrider-reborn]
    - add input timeout of 1.5s since we don't want to boost DDR for
    zygote forks not initiated by user input, such as services
    spawning in background
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Change-Id: I117840fd96a8335ffcf35d4e0ae2c550a3c5a065

--- kernel/fork.c
+++ kernel/fork.c
@@ -97,6 +97,7 @@
 #include <linux/stackleak.h>
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
+#include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2415,6 +2416,12 @@ long _do_fork(struct kernel_clone_args *args)
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+
+	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
+	if (task_is_zygote(current) && df_boost_within_input(1500)) {
+		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 50);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+	}
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
