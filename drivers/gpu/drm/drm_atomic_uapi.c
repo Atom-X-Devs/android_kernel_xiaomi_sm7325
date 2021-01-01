d359996e03afcb1f5baf2cf490c013b89b204abf
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-31 20:29:30 -0800

    drm_atomic: Boost DDR bus upon running an atomic ioctl
    In order to reduce jank, request a DDR bus boost whenever an atomic
    ioctl is sent, which is what our diplay HAL uses to commit frames to the
    display. The boost should be sufficient enough to render the display's
    maximum FPS without any dropped frames when there is no significant
    external source of load.
    
    [ghostrider-reborn]:
    - Boost if only within 3.2s input timeout
    - This prevents unnecessary boosting such as during video playback
    - More details: https://github.com/caf-ginkgo/kernel_msm-4.14/commit/4ccf7265c0273f01c7fda6ba332ea78209bf9194
    
    Change-Id: Iaf2345c63554bfeb447a53168f1ff2487543c662
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/gpu/drm/drm_atomic_uapi.c
+++ drivers/gpu/drm/drm_atomic_uapi.c
@@ -33,6 +33,7 @@
 #include <drm/drm_writeback.h>
 #include <drm/drm_vblank.h>
 
+#include <linux/devfreq_boost.h>
 #include <linux/dma-fence.h>
 #include <linux/uaccess.h>
 #include <linux/sync_file.h>
@@ -1313,6 +1314,12 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev,
 	if ((arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
+
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
+			df_boost_within_input(3250)) {
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
+	}
 
 	state = drm_atomic_state_alloc(dev);
 	if (!state)
