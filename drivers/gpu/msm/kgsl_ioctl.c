01d3427d7855888ecd625d87653d9e9d92f795bc
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-24 18:02:14 -0800

    msm: kgsl: Reduce latency while processing ioctls
    The GPU's ioctls sleep a lot due to memory allocation, during which time
    the CPU can enter a deep idle state and take a while to finish executing
    the ioctl afterwards. We can alleviate this by optimistically assuming
    that the task running the ioctl won't migrate CPUs while it runs and
    restricting the current CPU to shallow idle states.
    
    Change-Id: I80c3305eb47c3c51dbf9fbae50b091a653d9f48b
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/gpu/msm/kgsl_ioctl.c
+++ drivers/gpu/msm/kgsl_ioctl.c
@@ -3,6 +3,8 @@
  * Copyright (c) 2008-2021, The Linux Foundation. All rights reserved.
  */
 
+#include <linux/pm_qos.h>
+
 #include "kgsl_device.h"
 #include "kgsl_sync.h"
 #include "adreno.h"
@@ -163,7 +165,8 @@ long kgsl_ioctl_helper(struct file *filep, unsigned int cmd, unsigned long arg,
 	return ret;
 }
 
-long kgsl_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
+static long __kgsl_ioctl(struct file *filep, unsigned int cmd,
+			 unsigned long arg)
 {
 	struct kgsl_device_private *dev_priv = filep->private_data;
 	struct kgsl_device *device = dev_priv->device;
@@ -188,6 +191,27 @@ long kgsl_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
 		else if (device->ftbl->ioctl != NULL)
 			return device->ftbl->ioctl(dev_priv, cmd, arg);
 	}
+
+	return ret;
+}
+
+long kgsl_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
+{
+	/*
+	 * Optimistically assume the current task won't migrate to another CPU
+	 * and restrict the current CPU to shallow idle states so that it won't
+	 * take too long to finish running the ioctl whenever the ioctl runs a
+	 * command that sleeps, such as for memory allocation.
+	 */
+	struct pm_qos_request req = {
+		.type = PM_QOS_REQ_AFFINE_CORES,
+		.cpus_affine = BIT(raw_smp_processor_id())
+	};
+	long ret;
+
+	cpu_latency_qos_add_request(&req, 100);
+	ret = __kgsl_ioctl(filep, cmd, arg);
+	cpu_latency_qos_remove_request(&req);
 
 	return ret;
 }
