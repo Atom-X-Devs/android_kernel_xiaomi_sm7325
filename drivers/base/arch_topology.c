ad638ce60f7e8b88d820b12f735145d58ea544cf
Viresh Kumar <viresh.kumar@linaro.org>
2021-03-10 08:16:40 +0530

    arch_topology: Rename freq_scale as arch_freq_scale
    Rename freq_scale to a less generic name, as it will get exported soon
    for modules. Since x86 already names its own implementation of this as
    arch_freq_scale, lets stick to that.
    
    Suggested-by: Will Deacon <will@kernel.org>
    Change-Id: I539d417ba682dd13f4f9496b6a0e46da185d3c03
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/base/arch_topology.c
+++ drivers/base/arch_topology.c
@@ -33,7 +33,7 @@ __weak bool arch_freq_counters_available(const struct cpumask *cpus)
 {
 	return false;
 }
-DEFINE_PER_CPU(unsigned long, freq_scale) = SCHED_CAPACITY_SCALE;
+DEFINE_PER_CPU(unsigned long, arch_freq_scale) = SCHED_CAPACITY_SCALE;
 DEFINE_PER_CPU(unsigned long, max_cpu_freq);
 DEFINE_PER_CPU(unsigned long, max_freq_scale) = SCHED_CAPACITY_SCALE;
 
@@ -59,7 +59,7 @@ void arch_set_freq_scale(const struct cpumask *cpus, unsigned long cur_freq,
 	trace_android_vh_arch_set_freq_scale((struct cpumask *)cpus, cur_freq, max_freq, &scale);
 
 	for_each_cpu(i, cpus){
-		per_cpu(freq_scale, i) = scale;
+		per_cpu(arch_freq_scale, i) = scale;
 		per_cpu(max_cpu_freq, i) = max_freq;
 	}
 }
