68ba87a67c21a8cf43dfdb600ed3c4f00a8a79bf
Viresh Kumar <viresh.kumar@linaro.org>
2021-03-10 08:21:04 +0530

    arch_topology: Allow multiple entities to provide sched_freq_tick() callback
    This patch attempts to make it generic enough so other parts of the
    kernel can also provide their own implementation of scale_freq_tick()
    callback, which is called by the scheduler periodically to update the
    per-cpu arch_freq_scale variable.
    
    The implementations now need to provide 'struct scale_freq_data' for the
    CPUs for which they have hardware counters available, and a callback
    gets registered for each possible CPU in a per-cpu variable.
    
    The arch specific (or ARM AMU) counters are updated to adapt to this and
    they take the highest priority if they are available, i.e. they will be
    used instead of CPPC based counters for example.
    
    The special code to rebuild the sched domains, in case invariance status
    change for the system, is moved out of arm64 specific code and is added
    to arch_topology.c.
    
    Note that this also defines SCALE_FREQ_SOURCE_CPUFREQ but doesn't use it
    and it is added to show that cpufreq is also acts as source of
    information for FIE and will be used by default if no other counters are
    supported for a platform.
    
    Reviewed-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Tested-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Acked-by: Will Deacon <will@kernel.org> # for arm64
    Tested-by: Vincent Guittot <vincent.guittot@linaro.org>
    Change-Id: Ic44bbac625228a2fabb3503338738501d3cb3194
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- include/linux/arch_topology.h
+++ include/linux/arch_topology.h
@@ -41,6 +41,20 @@ unsigned long topology_get_max_freq_scale(struct sched_domain *sd, int cpu)
 	return per_cpu(max_freq_scale, cpu);
 }
 
+enum scale_freq_source {
+	SCALE_FREQ_SOURCE_CPUFREQ = 0,
+	SCALE_FREQ_SOURCE_ARCH,
+};
+
+struct scale_freq_data {
+	enum scale_freq_source source;
+	void (*set_freq_scale)(void);
+};
+
+void topology_scale_freq_tick(void);
+void topology_set_scale_freq_source(struct scale_freq_data *data, const struct cpumask *cpus);
+void topology_clear_scale_freq_source(enum scale_freq_source source, const struct cpumask *cpus);
+
 void topology_set_freq_scale(const struct cpumask *cpus, unsigned long cur_freq,
 			     unsigned long max_freq);
 
