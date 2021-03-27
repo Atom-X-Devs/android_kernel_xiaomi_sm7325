d918f93b94f5f08009e800bf9e14aa75ec601cbd
Ray Chi <raychi@google.com>
2021-03-28 02:17:42 +0800

    usb: dwc3: add cancelled reasons for dwc3 requests
    Currently, when dwc3 handles request cancelled, dwc3 just returns
    -ECONNRESET for all requests. It will cause USB function drivers
    can't know if the requests are cancelled by other reasons.
    
    This patch will replace DWC3_REQUEST_STATUS_CANCELLED with the
    reasons below.
      - DWC3_REQUEST_STATUS_DISCONNECTED
      - DWC3_REQUEST_STATUS_DEQUEUED
      - DWC3_REQUEST_STATUS_STALLED
    
    Reviewed-by: Thinh Nguyen <Thinh.Nguyen@synopsys.com>
    Change-Id: I6abc58274432e8c484974612d66eb3b55121bd42
    Signed-off-by: Ray Chi <raychi@google.com>
    Link: https://lore.kernel.org/r/20210327181742.1810969-1-raychi@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1531,7 +1531,7 @@ static int __dwc3_gadget_kick_transfer(struct dwc3_ep *dep)
 		dwc3_stop_active_transfer(dep, true, true);
 
 		list_for_each_entry_safe(req, tmp, &dep->started_list, list)
-			dwc3_gadget_move_cancelled_request(req);
+			dwc3_gadget_move_cancelled_request(req, DWC3_REQUEST_STATUS_DEQUEUED);
 		/* If ep isn't started, then there's no end transfer pending */
 		if (!(dep->flags & DWC3_EP_END_TRANSFER_PENDING))
 			dwc3_gadget_ep_cleanup_cancelled_requests(dep);
@@ -1850,13 +1850,27 @@ static void dwc3_gadget_ep_skip_trbs(struct dwc3_ep *dep, struct dwc3_request *r
 static void dwc3_gadget_ep_cleanup_cancelled_requests(struct dwc3_ep *dep)
 {
 	struct dwc3_request		*req;
-	struct dwc3			*dwc = dep->dwc;
+	struct dwc3				*dwc = dep->dwc;
 	int				request_count = 0;
 
 	while (!list_empty(&dep->cancelled_list)) {
 		req = next_request(&dep->cancelled_list);
 		dwc3_gadget_ep_skip_trbs(dep, req);
-		dwc3_gadget_giveback(dep, req, -ECONNRESET);
+		switch (req->status) {
+		case DWC3_REQUEST_STATUS_DISCONNECTED:
+			dwc3_gadget_giveback(dep, req, -ESHUTDOWN);
+			break;
+		case DWC3_REQUEST_STATUS_DEQUEUED:
+			dwc3_gadget_giveback(dep, req, -ECONNRESET);
+			break;
+		case DWC3_REQUEST_STATUS_STALLED:
+			dwc3_gadget_giveback(dep, req, -EPIPE);
+			break;
+		default:
+			dev_err(dwc->dev, "request cancelled with wrong reason:%d\n", req->status);
+			dwc3_gadget_giveback(dep, req, -ECONNRESET);
+			break;
+		}
 		request_count++;
 	}
 
@@ -1928,7 +1942,8 @@ static int dwc3_gadget_ep_dequeue(struct usb_ep *ep,
 			 * cancelled.
 			 */
 			list_for_each_entry_safe(r, t, &dep->started_list, list)
-				dwc3_gadget_move_cancelled_request(r);
+				dwc3_gadget_move_cancelled_request(r,
+						DWC3_REQUEST_STATUS_DEQUEUED);
 
 			/* If ep isn't started, then there's no end transfer
 			 * pending
