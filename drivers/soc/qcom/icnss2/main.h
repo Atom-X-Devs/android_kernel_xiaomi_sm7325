1d7aa860a25de9211874132ca3d8d07b3279f52f
Sandeep Singh <sandsing@codeaurora.org>
2021-07-06 22:30:18 +0530

    icnss2: Add new pdr framework support for icnss2 driver
    Add new pdr framework support for icnss2 driver.
    
    Change-Id: I114b2c3e34e2d431cfd4d38e5ea69f37168ef0f0
    Signed-off-by: Sandeep Singh <sandsing@codeaurora.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/soc/qcom/icnss2/main.h
+++ drivers/soc/qcom/icnss2/main.h
@@ -16,8 +16,6 @@
 #include <linux/power_supply.h>
 #include <dt-bindings/iio/qcom,spmi-vadc.h>
 #include <soc/qcom/icnss2.h>
-#include <soc/qcom/service-locator.h>
-#include <soc/qcom/service-notifier.h>
 #include "wlan_firmware_service_v01.h"
 #include <linux/timer.h>
 
@@ -295,12 +293,6 @@ struct icnss_stats {
 #define WLFW_MAX_NUM_SHADOW_REG 24
 #define WLFW_MAX_HANG_EVENT_DATA_SIZE 400
 
-struct service_notifier_context {
-	void *handle;
-	uint32_t instance_id;
-	char name[QMI_SERVREG_LOC_NAME_LENGTH_V01 + 1];
-};
-
 struct wlfw_rf_chip_info {
 	uint32_t chip_id;
 	uint32_t chip_family;
@@ -413,11 +405,6 @@ struct icnss_priv {
 	struct dentry *root_dentry;
 	spinlock_t on_off_lock;
 	struct icnss_stats stats;
-	struct work_struct service_notifier_work;
-	struct service_notifier_context *service_notifier;
-	struct notifier_block service_notifier_nb;
-	int total_domains;
-	struct notifier_block get_service_nb;
 	void *modem_notify_handler;
 	void *wpss_notify_handler;
 	struct notifier_block modem_ssr_nb;
@@ -471,6 +458,9 @@ struct icnss_priv {
 	u32 hw_trc_override;
 	struct icnss_dms_data dms;
 	u8 use_nv_mac;
+	struct pdr_handle *pdr_handle;
+	struct pdr_service *pdr_service;
+	bool root_pd_shutdown;
 	u32 wlan_en_delay_ms;
 	bool psf_supported;
 	struct notifier_block psf_nb;
