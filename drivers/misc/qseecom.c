266cc8be7b02d67972c73f1fbbd6d64ef3562145
Anmolpreet Kaur <anmolpre@codeaurora.org>
2021-07-16 10:31:48 +0530

    qseecom: Release table when listener is not available
    It might happen that secure side requests for listener
    buut its currently not free and blocked. When listener
    becomes available, it gets killed due to reboot scenario
    of test case. In this case we need to make table as NULL
    as well because it contains some value from previous iteration.
    
    Change-Id: I4aef91a41c4fd3fbb545551dd2a08c0a89bc0277
    Signed-off-by: Anmolpreet Kaur <anmolpre@codeaurora.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -2533,6 +2533,7 @@ static int __qseecom_reentrancy_process_incomplete_cmd(
 						lstnr);
 			rc = -ERESTARTSYS;
 			ptr_svc = NULL;
+			table = NULL;
 			status = QSEOS_RESULT_FAILURE;
 			goto err_resp;
 		}
