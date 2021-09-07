606bdbfb080b962a3b682c0459510d373363b0da
Shaleen Agrawal <shalagra@codeaurora.org>
2021-09-07 11:33:03 -0700

    sched: walt: configure the default rtg boost freq for cpu4
    Change-Id: I000f0d6aa486da31cf805f2aa1d21a821f6ebfdc
    Signed-off-by: Shaleen Agrawal <shalagra@codeaurora.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -518,7 +518,7 @@ static inline bool sugov_cpu_is_busy(struct sugov_cpu *sg_cpu) { return false; }
 #define NL_RATIO 75
 #define DEFAULT_HISPEED_LOAD 90
 #define DEFAULT_CPU0_RTG_BOOST_FREQ 1000000
-#define DEFAULT_CPU4_RTG_BOOST_FREQ 0
+#define DEFAULT_CPU4_RTG_BOOST_FREQ 768000
 #define DEFAULT_CPU7_RTG_BOOST_FREQ 0
 static void sugov_walt_adjust(struct sugov_cpu *sg_cpu, unsigned long *util,
 			      unsigned long *max)
