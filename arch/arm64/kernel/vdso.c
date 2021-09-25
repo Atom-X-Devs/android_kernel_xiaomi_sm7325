62d43298415f0c79b3745a725d0d791780b27a31
Will Deacon <will@kernel.org>
2021-09-25 17:05:00 +0530

    arm64: vdso: Fix unusual formatting in *setup_additional_pages()
    There's really no need to put every parameter on a new line when calling
    a function with a long name, so reformat the *setup_additional_pages()
    functions in the vDSO setup code to follow the usual conventions.
    
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Change-Id: I862531295e6398fc3917cad0d7f669018028db17
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- arch/arm64/kernel/vdso.c
+++ arch/arm64/kernel/vdso.c
@@ -327,9 +327,7 @@ int aarch32_setup_additional_pages(struct linux_binprm *bprm, int uses_interp)
 		goto out;
 
 	if (IS_ENABLED(CONFIG_COMPAT_VDSO)) {
-		ret = __setup_additional_pages(VDSO_ABI_AA32,
-					       mm,
-					       bprm,
+		ret = __setup_additional_pages(VDSO_ABI_AA32, mm, bprm,
 					       uses_interp);
 
 		if (ret)
@@ -373,8 +371,7 @@ static int __init vdso_init(void)
 }
 arch_initcall(vdso_init);
 
-int arch_setup_additional_pages(struct linux_binprm *bprm,
-				int uses_interp)
+int arch_setup_additional_pages(struct linux_binprm *bprm, int uses_interp)
 {
 	struct mm_struct *mm = current->mm;
 	int ret;
@@ -382,11 +379,7 @@ int arch_setup_additional_pages(struct linux_binprm *bprm,
 	if (down_write_killable(&mm->mmap_sem))
 		return -EINTR;
 
-	ret = __setup_additional_pages(VDSO_ABI_AA64,
-				       mm,
-				       bprm,
-				       uses_interp);
-
+	ret = __setup_additional_pages(VDSO_ABI_AA64, mm, bprm, uses_interp);
 	up_write(&mm->mmap_sem);
 
 	return ret;
