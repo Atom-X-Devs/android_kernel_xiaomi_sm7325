b2d92a73fc8f806f06dcfb83e900402035b04b02
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2021-10-22 18:04:02 +0200

    PM: suspend: Do not pause cpuidle in the suspend-to-idle path
    It is pointless to pause cpuidle in the suspend-to-idle path,
    because it is going to be resumed in the same path later and
    pausing it does not serve any particular purpose in that case.
    
    Rework the code to avoid doing that.
    
    Change-Id: I9ae049a4d795568f4202c0da6a36c2f8a4ce7ae8
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Tested-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -803,8 +803,6 @@ void dpm_resume_noirq(pm_message_t state)
 
 	resume_device_irqs();
 	device_wakeup_disarm_wake_irqs();
-
-	cpuidle_resume();
 }
 
 static pm_callback_t dpm_subsys_resume_early_cb(struct device *dev,
@@ -946,6 +944,7 @@ void dpm_resume_early(pm_message_t state)
 void dpm_resume_start(pm_message_t state)
 {
 	dpm_resume_noirq(state);
+	cpuidle_resume();
 	dpm_resume_early(state);
 }
 EXPORT_SYMBOL_GPL(dpm_resume_start);
@@ -1446,8 +1445,6 @@ int dpm_suspend_noirq(pm_message_t state)
 {
 	int ret;
 
-	cpuidle_pause();
-
 	device_wakeup_arm_wake_irqs();
 	suspend_device_irqs();
 
@@ -1651,9 +1648,13 @@ int dpm_suspend_end(pm_message_t state)
 	if (error)
 		goto out;
 
+	cpuidle_pause();
+
 	error = dpm_suspend_noirq(state);
-	if (error)
+	if (error) {
+		cpuidle_resume();
 		dpm_resume_early(resume_event(state));
+	}
 
 out:
 	dpm_show_time(starttime, state, error, "end");
