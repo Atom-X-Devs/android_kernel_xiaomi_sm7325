61d33a9ff7ae2cb02ea576e8f37f3b6f7fde4537
Yaroslav Furman <yaro330@gmail.com>
2021-12-31 19:05:40 +0300

    drivers: thermal: compile out debugfs
    Change-Id: Icec9caa0928e6910f5cce2d8562de293c85b5187
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: atndko <z1281552865@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1802,7 +1802,9 @@ static int __init thermal_init(void)
 	if (result)
 		pr_warn("Thermal: Can not register suspend notifier, return %d\n",
 			result);
+#ifdef CONFIG_DEBUG_FS
 	thermal_debug_init();
+#endif
 
 	return 0;
 
@@ -1828,7 +1830,9 @@ static void thermal_exit(void)
 	destroy_workqueue(thermal_passive_wq);
 	genetlink_exit();
 	class_unregister(&thermal_class);
+#ifdef CONFIG_DEBUG_FS
 	thermal_debug_exit();
+#endif
 	thermal_unregister_governors();
 	ida_destroy(&thermal_tz_ida);
 	ida_destroy(&thermal_cdev_ida);
