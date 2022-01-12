9da19cb97947ab74e5427f9731e112709601f87b
Divyanshu-Modi <divyan.m05@gmail.com>
2022-01-12 11:52:01 +0530

    net: import XIAOMI changes
    From lisa-r-oss
    
    Change-Id: I1a526ef7869cf5210c0b73af40b92e2fee4ca4f8
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -994,6 +994,10 @@ int udp_sendmsg(struct sock *sk, struct msghdr *msg, size_t len)
 	int (*getfrag)(void *, char *, int, int, int, struct sk_buff *);
 	struct sk_buff *skb;
 	struct ip_options_data opt_copy;
+#ifdef CONFIG_MACH_XIAOMI
+	__be16 tmp_dport;
+	__be32 tmp_daddr;
+#endif
 
 	if (len > 0xFFFF)
 		return -EMSGSIZE;
@@ -1038,6 +1042,15 @@ int udp_sendmsg(struct sock *sk, struct msghdr *msg, size_t len)
 
 		daddr = usin->sin_addr.s_addr;
 		dport = usin->sin_port;
+#ifdef CONFIG_MACH_XIAOMI
+		tmp_dport = sk->sk_dport;
+		tmp_daddr = sk->sk_daddr;
+		sk->sk_dport = dport;
+		sk->sk_daddr = daddr;
+		udp_state_bpf(sk);
+		sk->sk_dport = tmp_dport;
+		sk->sk_daddr = tmp_daddr;
+#endif
 		if (dport == 0)
 			return -EINVAL;
 	} else {
