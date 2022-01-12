cd2f73da497af764c58887518d3e12fa7ea3af38
Divyanshu-Modi <divyan.m05@gmail.com>
2022-01-12 14:11:08 +0530

    Kernel: import XIAOMI changes
    From lisa-r-oss
    
    Change-Id: Ie24216109c40884ac1ff03ba4e0e01a2c1f1c510
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- kernel/module.c
+++ kernel/module.c
@@ -1432,6 +1432,7 @@ static int verify_namespace_is_imported(const struct load_info *info,
 	return 0;
 }
 
+#ifndef CONFIG_MACH_XIAOMI
 static bool inherit_taint(struct module *mod, struct module *owner)
 {
 	if (!owner || !test_bit(TAINT_PROPRIETARY_MODULE, &owner->taints))
@@ -1450,6 +1451,7 @@ static bool inherit_taint(struct module *mod, struct module *owner)
 	}
 	return true;
 }
+#endif
 
 /* Resolve a symbol for this module.  I.e. if we find one, record usage. */
 static const struct kernel_symbol *resolve_symbol(struct module *mod,
@@ -1475,6 +1477,7 @@ static const struct kernel_symbol *resolve_symbol(struct module *mod,
 	if (!sym)
 		goto unlock;
 
+#ifndef CONFIG_MACH_XIAOMI
 	if (license == GPL_ONLY)
 		mod->using_gplonly_symbols = true;
 
@@ -1482,6 +1485,7 @@ static const struct kernel_symbol *resolve_symbol(struct module *mod,
 		sym = NULL;
 		goto getname;
 	}
+#endif
 
 	if (!check_version(info, name, mod, crc)) {
 		sym = ERR_PTR(-EINVAL);
