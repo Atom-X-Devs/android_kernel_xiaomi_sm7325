93109093a77472f7bc1020009037561c0545d264
Divyanshu-Modi <divyan.m05@gmail.com>
2022-01-13 17:57:34 +0530

    firmware_loader: import xiaomi changes
    Change-Id: I2a0ded4434f521686ccd8df35bbabfcd60399c9f
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/base/firmware_loader/main.c
+++ drivers/base/firmware_loader/main.c
@@ -445,6 +445,9 @@ static int fw_decompress_xz(struct device *dev, struct fw_priv *fw_priv,
 static char fw_path_para[256];
 static const char * const fw_path[] = {
 	fw_path_para,
+#ifdef CONFIG_MACH_XIAOMI
+	"/vendor/firmware",
+#endif
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
