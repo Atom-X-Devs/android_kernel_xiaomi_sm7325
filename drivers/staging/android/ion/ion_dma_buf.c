96874ec87db4ea9b5a879d3ec5b986aa3db1f8ec
Sultan Alsawaf <sultan@kerneltoast.com>
2022-01-17 21:24:31 +0900

    iommu: msm: Rewrite to improve clarity and performance
    This scope of this driver's lock usage is extremely wide, leading to
    excessively long lock hold times. Additionally, there is lots of
    excessive linked-list traversal and unnecessary dynamic memory
    allocation in a critical path, causing poor performance across the
    board.
    
    Fix all of this by greatly reducing the scope of the locks used and by
    significantly reducing the amount of operations performed when
    msm_dma_map_sg_attrs() is called. The entire driver's code is overhauled
    for better cleanliness and performance.
    
    Note that ION must be modified to pass a known structure via the private
    dma_buf pointer, so that the IOMMU driver can prevent races when
    operating on the same buffer concurrently. This is the only way to
    eliminate said buffer races without hurting the IOMMU driver's
    performance.
    
    Some additional members are added to the device struct as well to make
    these various performance improvements possible.
    
    This also removes the manual cache maintenance since ION already handles
    it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [Kazuki: Port to v5.4]
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Change-Id: I9b42831d5e6310f0806cdbcd0fae86cd57d6e873
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/staging/android/ion/ion_dma_buf.c
+++ drivers/staging/android/ion/ion_dma_buf.c
@@ -16,6 +16,8 @@
 
 #include "ion_private.h"
 
+#define to_iommu_data(x) container_of(x, typeof(*buffer), iommu_data)
+
 static struct sg_table *dup_sg_table(struct sg_table *table)
 {
 	struct sg_table *new_table;
@@ -53,7 +55,7 @@ static int ion_dma_buf_attach(struct dma_buf *dmabuf,
 {
 	struct ion_dma_buf_attachment *a;
 	struct sg_table *table;
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 
 	if (heap->buf_ops.attach)
@@ -87,7 +89,7 @@ static void ion_dma_buf_detatch(struct dma_buf *dmabuf,
 				struct dma_buf_attachment *attachment)
 {
 	struct ion_dma_buf_attachment *a = attachment->priv;
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 
 	if (heap->buf_ops.detach)
@@ -104,7 +106,7 @@ static void ion_dma_buf_detatch(struct dma_buf *dmabuf,
 static struct sg_table *ion_map_dma_buf(struct dma_buf_attachment *attachment,
 					enum dma_data_direction direction)
 {
-	struct ion_buffer *buffer = attachment->dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(attachment->dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 	struct ion_dma_buf_attachment *a;
 	struct sg_table *table;
@@ -132,7 +134,7 @@ static void ion_unmap_dma_buf(struct dma_buf_attachment *attachment,
 			      struct sg_table *table,
 			      enum dma_data_direction direction)
 {
-	struct ion_buffer *buffer = attachment->dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(attachment->dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 	struct ion_dma_buf_attachment *a = attachment->priv;
 	unsigned long attrs = attachment->dma_map_attrs;
@@ -152,7 +154,7 @@ static void ion_unmap_dma_buf(struct dma_buf_attachment *attachment,
 
 static void ion_dma_buf_release(struct dma_buf *dmabuf)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 
 #ifdef CONFIG_MACH_XIAOMI
@@ -167,7 +169,7 @@ static void ion_dma_buf_release(struct dma_buf *dmabuf)
 static int ion_dma_buf_begin_cpu_access(struct dma_buf *dmabuf,
 					enum dma_data_direction direction)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 	struct ion_dma_buf_attachment *a;
 
@@ -195,7 +197,7 @@ ion_dma_buf_begin_cpu_access_partial(struct dma_buf *dmabuf,
 				     enum dma_data_direction direction,
 				     unsigned int offset, unsigned int len)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 
 	/* This is done to make sure partial buffer cache flush / invalidate is
@@ -212,7 +214,7 @@ ion_dma_buf_begin_cpu_access_partial(struct dma_buf *dmabuf,
 static int ion_dma_buf_end_cpu_access(struct dma_buf *dmabuf,
 				      enum dma_data_direction direction)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 	struct ion_dma_buf_attachment *a;
 
@@ -240,7 +242,7 @@ static int ion_dma_buf_end_cpu_access_partial(struct dma_buf *dmabuf,
 					      unsigned int offset,
 					      unsigned int len)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 
 	/* This is done to make sure partial buffer cache flush / invalidate is
@@ -256,7 +258,7 @@ static int ion_dma_buf_end_cpu_access_partial(struct dma_buf *dmabuf,
 
 static void *ion_dma_buf_map(struct dma_buf *dmabuf, unsigned long offset)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 
 	if (heap->buf_ops.map)
@@ -267,7 +269,7 @@ static void *ion_dma_buf_map(struct dma_buf *dmabuf, unsigned long offset)
 
 static int ion_dma_buf_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 	int ret;
 
@@ -293,7 +295,7 @@ static int ion_dma_buf_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma)
 static void ion_dma_buf_unmap(struct dma_buf *dmabuf, unsigned long offset,
 			      void *addr)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 
 	if (!heap->buf_ops.unmap)
@@ -303,7 +305,7 @@ static void ion_dma_buf_unmap(struct dma_buf *dmabuf, unsigned long offset,
 
 static void *ion_dma_buf_vmap(struct dma_buf *dmabuf)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 	void *vaddr;
 
@@ -319,7 +321,7 @@ static void *ion_dma_buf_vmap(struct dma_buf *dmabuf)
 
 static void ion_dma_buf_vunmap(struct dma_buf *dmabuf, void *vaddr)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 
 	if (heap->buf_ops.vunmap) {
@@ -334,7 +336,7 @@ static void ion_dma_buf_vunmap(struct dma_buf *dmabuf, void *vaddr)
 
 static int ion_dma_buf_get_flags(struct dma_buf *dmabuf, unsigned long *flags)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
+	struct ion_buffer *buffer = to_iommu_data(dmabuf->priv);
 	struct ion_heap *heap = buffer->heap;
 
 	if (!heap->buf_ops.get_flags)
@@ -385,7 +387,7 @@ struct dma_buf *ion_dmabuf_alloc(struct ion_device *dev, size_t len,
 	exp_info.ops = &dma_buf_ops;
 	exp_info.size = buffer->size;
 	exp_info.flags = O_RDWR;
-	exp_info.priv = buffer;
+	exp_info.priv = &buffer->iommu_data;
 #ifdef CONFIG_MACH_XIAOMI
 	exp_info.exp_name = kasprintf(GFP_KERNEL, "%s-%s-%d-%s", KBUILD_MODNAME,
 		buffer->heap->name, current->tgid, task_comm);
