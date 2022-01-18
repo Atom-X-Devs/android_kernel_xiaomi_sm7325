7537beb451434baa4d14326ad119a1dc5388e60c
Divyanshu-Modi <divyan.m05@gmail.com>
2022-01-18 12:56:04 +0530

    drivers: usb: import XIAOMI changes
    from lisa-r-oss
    
    Change-Id: I1799d1294a39b7087a64d44b110d6fd5a748e551
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/usb/dwc3/gadget.h
+++ drivers/usb/dwc3/gadget.h
@@ -95,11 +95,18 @@ static inline void dwc3_gadget_move_started_request(struct dwc3_request *req)
  * Caller should take care of locking. This function will move @req from its
  * current list to the endpoint's cancelled_list.
  */
+#ifndef CONFIG_MACH_XIAOMI
 static inline void dwc3_gadget_move_cancelled_request(struct dwc3_request *req)
+#else
+static inline void dwc3_gadget_move_cancelled_request(struct dwc3_request *req, unsigned int reason)
+#endif
 {
 	struct dwc3_ep		*dep = req->dep;
 
 	req->status = DWC3_REQUEST_STATUS_CANCELLED;
+#ifdef CONFIG_MACH_XIAOMI
+    req->status = reason;
+#endif
 	list_move_tail(&req->list, &dep->cancelled_list);
 }
 
