e954cb55d864a17514d884e4cfc5b9351dd3214a
Divyanshu-Modi <divyan.m05@gmail.com>
2022-04-02 11:18:39 +0530

    techpach: display: dsi: don't log on EPROBE_DEFER
    anyways it's gonna init so don't log it
    
    Change-Id: Ie8c9397405045ba5453b503dcaba69c51e47bc3f
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -4347,7 +4347,9 @@ static int dsi_display_res_init(struct dsi_display *display)
 		ctrl->ctrl = dsi_ctrl_get(ctrl->ctrl_of_node);
 		if (IS_ERR_OR_NULL(ctrl->ctrl)) {
 			rc = PTR_ERR(ctrl->ctrl);
-			DSI_ERR("failed to get dsi controller, rc=%d\n", rc);
+			if (rc != -EPROBE_DEFER)
+				DSI_ERR("failed to get dsi controller, rc=%d\n", rc);
+
 			ctrl->ctrl = NULL;
 			goto error_ctrl_put;
 		}
@@ -5430,8 +5432,10 @@ static int _dsi_display_dev_init(struct dsi_display *display)
 
 	rc = dsi_display_res_init(display);
 	if (rc) {
-		DSI_ERR("[%s] failed to initialize resources, rc=%d\n",
-		       display->name, rc);
+		if (rc != -EPROBE_DEFER)
+			DSI_ERR("[%s] failed to initialize resources, rc=%d\n",
+			       display->name, rc);
+
 		goto error;
 	}
 error:
@@ -6029,7 +6033,9 @@ static int dsi_display_init(struct dsi_display *display)
 
 	rc = _dsi_display_dev_init(display);
 	if (rc) {
-		DSI_ERR("device init failed, rc=%d\n", rc);
+		if (rc != -EPROBE_DEFER)
+			DSI_ERR("device init failed, rc=%d\n", rc);
+
 		goto end;
 	}
 
