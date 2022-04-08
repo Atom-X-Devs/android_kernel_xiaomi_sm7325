2036f45b7953698fccc1591d9a579f3b8dbc8137
Divyanshu-Modi <divyan.m05@gmail.com>
2022-04-08 17:32:20 +0530

    drivers: input: aw8624: Call cpu_latency_qos_*() instead of pm_qos_*()
    Call cpu_latency_qos_add/remove_request() instead of
    pm_qos_add/remove_request(), respectively, because the
    latter are going to be dropped.
    
    No intentional functional impact.
    
    Change-Id: I99d17446026547709279f3c55c2f951dec79dcc0
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/input/misc/aw8624_haptic/aw8624.c
+++ drivers/input/misc/aw8624_haptic/aw8624.c
@@ -1076,8 +1076,7 @@ static int aw8624_haptic_rtp_init(struct aw8624 *aw8624)
 	unsigned int buf_len = 0;
 	bool rtp_start = true;
 
-	pm_qos_add_request(&pm_qos_req_vb, PM_QOS_CPU_DMA_LATENCY,
-			   PM_QOS_VALUE_VB);
+	cpu_latency_qos_add_request(&pm_qos_req_vb, PM_QOS_VALUE_VB);
 	aw8624->rtp_cnt = 0;
 	if (aw8624->irq_sts_flag) {
 		disable_irq(gpio_to_irq(aw8624->irq_gpio));
@@ -1130,7 +1129,7 @@ static int aw8624_haptic_rtp_init(struct aw8624 *aw8624)
 	}
 
 	pr_debug("%s: exit\n", __func__);
-	pm_qos_remove_request(&pm_qos_req_vb);
+	cpu_latency_qos_remove_request(&pm_qos_req_vb);
 	return 0;
 }
 
@@ -1364,8 +1363,7 @@ static int aw8624_rtp_osc_calibration(struct aw8624 *aw8624)
 	}
 	/* haptic start */
 	aw8624_haptic_start(aw8624);
-	pm_qos_add_request(&pm_qos_req_vb, PM_QOS_CPU_DMA_LATENCY,
-			   PM_QOS_VALUE_VB);
+	cpu_latency_qos_add_request(&pm_qos_req_vb, PM_QOS_VALUE_VB);
 	while (1) {
 		if (!aw8624_haptic_rtp_get_fifo_afi(aw8624)) {
 			pr_info
@@ -1410,7 +1408,7 @@ static int aw8624_rtp_osc_calibration(struct aw8624 *aw8624)
 			break;
 		}
 	}
-	pm_qos_remove_request(&pm_qos_req_vb);
+	cpu_latency_qos_remove_request(&pm_qos_req_vb);
 	if (!aw8624->irq_sts_flag) {
 		enable_irq(gpio_to_irq(aw8624->irq_gpio));
 		aw8624->irq_sts_flag = 1;
