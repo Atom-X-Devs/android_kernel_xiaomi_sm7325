a4090af467661897d48968f2936e460f8d75a4d6
Anil Veshala Veshala <quic_aveshala@quicinc.com>
2022-04-12 07:31:22 -0700

    platform: msm: qcom-geni-se: add synchronization b/w list_add and list_del
    For client drivers spi/i2c/uart/i3c, if resume/suspend fails
    due to command timeout or anyother reasons we are returning
    error after clock voting is done, if client initiate the
    next xfer, we are doing clock voting again, due to this we
    are calling list_add again without doing list_del, this is
    causing NULL pointer access. To solve this added synchronization
    logic b/w list_add and list_del.
    
    Change-Id: Ia0ec40eaf22bddfef31ddbc8f39c80b5bb53a9a9
    Signed-off-by: Anil Veshala Veshala <quic_aveshala@quicinc.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- include/linux/msm-geni-se.h
+++ include/linux/msm-geni-se.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _LINUX_MSM_GENI_SE
@@ -75,6 +76,7 @@ struct se_geni_rsc {
 	unsigned int num_clk_levels;
 	unsigned long *clk_perf_tbl;
 	bool skip_bw_vote;
+	bool is_list_add;
 };
 
 #define PINCTRL_DEFAULT	"default"
