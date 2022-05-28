80d9e4f9bea3fa62002ef2fb9c0e077bb31f257c
Divyanshu-Modi <divyan.m05@gmail.com>
2022-05-28 21:15:07 +0530

    techpack:  Use clang-format to fix qcom cancer
     God! qcom........... is............ idk
    
    Change-Id: I03b1e2d49de8621fbdab62caa839813d73bc7c2f
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- techpack/datarmnet-ext/offload/rmnet_offload_state.h
+++ techpack/datarmnet-ext/offload/rmnet_offload_state.h
@@ -16,15 +16,28 @@
 #include "rmnet_map.h"
 #include "rmnet_descriptor.h"
 #include "qmi_rmnet.h"
-enum{DATARMNET0529bb9c4e,DATARMNET8e1c5c09f8,DATARMNETb881657565,};extern void(*
-rmnet_perf_desc_entry)(struct rmnet_frag_descriptor*DATARMNET9d1b321642,struct 
-rmnet_port*DATARMNETf0d9de7e2f);extern void(*rmnet_perf_chain_end)(void);struct 
-DATARMNET74bb35b8f2{struct rmnet_map_dl_ind DATARMNET8633ae8f1e;u32 
-DATARMNETe2251ce433;u32 DATARMNET7f59b108db;bool DATARMNETc2832b8f85;bool 
-DATARMNETb165d2c5c4;};struct DATARMNET70f3b87b5d{struct rmnet_port*
-DATARMNET403589239f;struct DATARMNET74bb35b8f2 DATARMNETa9f2b2f677;struct 
-qmi_rmnet_ps_ind DATARMNET341393eda2;struct DATARMNET907d58c807 
-DATARMNETebb45c8d86;u8 DATARMNETcb2b3ae8e4;};struct DATARMNET70f3b87b5d*
-DATARMNETc2a630b113(void);
+enum {
+	DATARMNET0529bb9c4e,
+	DATARMNET8e1c5c09f8,
+	DATARMNETb881657565,
+};
+extern void (*rmnet_perf_desc_entry)(
+	struct rmnet_frag_descriptor *DATARMNET9d1b321642,
+	struct rmnet_port *DATARMNETf0d9de7e2f);
+extern void (*rmnet_perf_chain_end)(void);
+struct DATARMNET74bb35b8f2 {
+	struct rmnet_map_dl_ind DATARMNET8633ae8f1e;
+	u32 DATARMNETe2251ce433;
+	u32 DATARMNET7f59b108db;
+	bool DATARMNETc2832b8f85;
+	bool DATARMNETb165d2c5c4;
+};
+struct DATARMNET70f3b87b5d {
+	struct rmnet_port *DATARMNET403589239f;
+	struct DATARMNET74bb35b8f2 DATARMNETa9f2b2f677;
+	struct qmi_rmnet_ps_ind DATARMNET341393eda2;
+	struct DATARMNET907d58c807 DATARMNETebb45c8d86;
+	u8 DATARMNETcb2b3ae8e4;
+};
+struct DATARMNET70f3b87b5d *DATARMNETc2a630b113(void);
 #endif
-
