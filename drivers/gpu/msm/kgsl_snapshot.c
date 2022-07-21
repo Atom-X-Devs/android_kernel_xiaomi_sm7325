edbd153857499b12beb0bc89febe4ec467463314
Divyanshu-Modi <divyan.m05@gmail.com>
2022-07-21 22:58:05 +0530

    Revert "msm: kgsl: Set snapshot_atomic only after passing all checks"
    This reverts commit df6dfd9a14b9ec9575c1aee1f1b29a688360455e.
    
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/gpu/msm/kgsl_snapshot.c
+++ drivers/gpu/msm/kgsl_snapshot.c
@@ -678,6 +678,8 @@ static void kgsl_device_snapshot_atomic(struct kgsl_device *device)
 	struct kgsl_snapshot *snapshot;
 	phys_addr_t pa;
 
+	device->snapshot_atomic = true;
+
 	if (device->snapshot && device->force_panic)
 		return;
 
@@ -715,7 +717,6 @@ static void kgsl_device_snapshot_atomic(struct kgsl_device *device)
 	if (snapshot == NULL)
 		return;
 
-	device->snapshot_atomic = true;
 	INIT_LIST_HEAD(&snapshot->obj_list);
 	INIT_LIST_HEAD(&snapshot->cp_list);
 
