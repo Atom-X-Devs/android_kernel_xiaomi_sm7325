bac9ffd223d95fd62ee7dc03949d0eb0583edc71
Divyanshu-Modi <divyan.m05@gmail.com>
2022-07-31 16:07:21 +0530

    Revert "time: sched_clock: record cycle count in suspend and resume"
    This reverts commit f121a71abbd1a46924a5cc33549f2d51bd743619.
    
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- kernel/time/sched_clock.c
+++ kernel/time/sched_clock.c
@@ -46,12 +46,6 @@ struct clock_data {
 static struct hrtimer sched_clock_timer;
 static int irqtime = -1;
 
-#ifdef CONFIG_PRINT_SUSPEND_EPOCH_QGKI
-static u64 suspend_ns;
-static u64 suspend_cycles;
-static u64 resume_cycles;
-#endif
-
 core_param(irqtime, irqtime, int, 0400);
 
 static u64 notrace jiffy_sched_clock_read(void)
@@ -271,13 +265,6 @@ int sched_clock_suspend(void)
 	struct clock_read_data *rd = &cd.read_data[0];
 
 	update_sched_clock();
-
-#ifdef CONFIG_PRINT_SUSPEND_EPOCH_QGKI
-	suspend_ns = rd->epoch_ns;
-	suspend_cycles = rd->epoch_cyc;
-	pr_info("suspend ns:%17llu      suspend cycles:%17llu\n",
-				rd->epoch_ns, rd->epoch_cyc);
-#endif
 	hrtimer_cancel(&sched_clock_timer);
 	rd->read_sched_clock = suspended_sched_clock_read;
 
@@ -290,10 +277,6 @@ void sched_clock_resume(void)
 
 	rd->epoch_cyc = cd.actual_read_sched_clock();
 	hrtimer_start(&sched_clock_timer, cd.wrap_kt, HRTIMER_MODE_REL_HARD);
-#ifdef CONFIG_PRINT_SUSPEND_EPOCH_QGKI
-	resume_cycles = rd->epoch_cyc;
-	pr_info("resume cycles:%17llu\n", rd->epoch_cyc);
-#endif
 	rd->read_sched_clock = cd.actual_read_sched_clock;
 }
 
