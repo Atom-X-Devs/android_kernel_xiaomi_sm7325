e723a6e552259d8831571e1793f494924cd2bf23
Divyanshu-Modi <divyan.m05@gmail.com>
2022-08-10 18:16:48 +0530

    interconnect: qcom: bcm-voter/rpmh: consolidate support
    changes brought in are from: https://github.com/arter97/caf_msm-5.10/commit/976daac4a1c581e5d5fd64047519fd6fcde39738
    
    Change-Id: I9a856fcfb5583b172010a067dab9ea62a40f7b6f
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/interconnect/qcom/bcm-voter.c
+++ drivers/interconnect/qcom/bcm-voter.c
@@ -1,13 +1,13 @@
-// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
  *
  */
+#include <asm/div64.h>
 #include <linux/interconnect-provider.h>
 #include <linux/list_sort.h>
 #include <linux/module.h>
 #include <linux/of.h>
-#include <linux/of_platform.h>
 #include <linux/platform_device.h>
 
 #include <soc/qcom/rpmh.h>
@@ -17,6 +17,7 @@
 #include "icc-rpmh.h"
 
 static LIST_HEAD(bcm_voters);
+static DEFINE_MUTEX(bcm_voter_lock);
 
 /**
  * struct bcm_voter - Bus Clock Manager voter
@@ -55,7 +56,7 @@ static int cmp_vcd(void *priv, struct list_head *a, struct list_head *b)
 		return 1;
 }
 
-static u64 bcm_div(u64 num, u64 base)
+static u64 bcm_div(u64 num, u32 base)
 {
 	/* Ensure that small votes aren't lost. */
 	if (num && num < base)
@@ -73,28 +74,24 @@ static void bcm_aggregate(struct qcom_icc_bcm *bcm, bool init)
 	u64 agg_avg[QCOM_ICC_NUM_BUCKETS] = {0};
 	u64 agg_peak[QCOM_ICC_NUM_BUCKETS] = {0};
 	u64 temp;
-	u32 bcm_width, bcm_unit;
-
-	bcm_width = le16_to_cpu(bcm->aux_data.width);
-	bcm_unit = le32_to_cpu(bcm->aux_data.unit);
 
 	for (bucket = 0; bucket < QCOM_ICC_NUM_BUCKETS; bucket++) {
 		for (i = 0; i < bcm->num_nodes; i++) {
 			node = bcm->nodes[i];
-			temp = bcm_div(node->sum_avg[bucket] * bcm_width,
+			temp = bcm_div(node->sum_avg[bucket] * bcm->aux_data.width,
 				       node->buswidth * node->channels);
 			agg_avg[bucket] = max(agg_avg[bucket], temp);
 
-			temp = bcm_div(node->max_peak[bucket] * bcm_width,
+			temp = bcm_div(node->max_peak[bucket] * bcm->aux_data.width,
 				       node->buswidth);
 			agg_peak[bucket] = max(agg_peak[bucket], temp);
 		}
 
 		temp = agg_avg[bucket] * bcm->vote_scale;
-		bcm->vote_x[bucket] = bcm_div(temp, bcm_unit);
+		bcm->vote_x[bucket] = bcm_div(temp, bcm->aux_data.unit);
 
 		temp = agg_peak[bucket] * bcm->vote_scale;
-		bcm->vote_y[bucket] = bcm_div(temp, bcm_unit);
+		bcm->vote_y[bucket] = bcm_div(temp, bcm->aux_data.unit);
 	}
 
 	if (bcm->keepalive) {
@@ -144,23 +141,22 @@ static inline void tcs_cmd_gen(struct tcs_cmd *cmd, u64 vote_x, u64 vote_y,
 
 static void tcs_list_gen(struct bcm_voter *voter, int bucket,
 			 struct tcs_cmd tcs_list[MAX_VCD],
-			 int n[MAX_VCD])
+			 int n[MAX_VCD + 1])
 {
-	struct qcom_icc_bcm *bcm;
 	struct list_head *bcm_list = &voter->commit_list;
+	struct qcom_icc_bcm *bcm;
 	bool commit, wait;
 	size_t idx = 0, batch = 0, cur_vcd_size = 0;
 
-	memset(n, 0, sizeof(int) * MAX_VCD);
+	memset(n, 0, sizeof(int) * (MAX_VCD + 1));
 
 	list_for_each_entry(bcm, bcm_list, list) {
 		commit = false;
 		cur_vcd_size++;
 		if ((list_is_last(&bcm->list, bcm_list)) ||
-		    bcm->aux_data.vcd !=
-			list_next_entry(bcm, list)->aux_data.vcd) {
-			cur_vcd_size = 0;
+		    bcm->aux_data.vcd != list_next_entry(bcm, list)->aux_data.vcd) {
 			commit = true;
+			cur_vcd_size = 0;
 		}
 
 		wait = commit && (voter->tcs_wait & BIT(bucket));
@@ -177,7 +173,7 @@ static void tcs_list_gen(struct bcm_voter *voter, int bucket,
 		 */
 		if (n[batch] >= MAX_RPMH_PAYLOAD) {
 			if (!commit) {
-				n[batch] = cur_vcd_size;
+				n[batch] -= cur_vcd_size;
 				n[batch + 1] = cur_vcd_size;
 			}
 			batch++;
@@ -209,24 +205,25 @@ struct bcm_voter *of_bcm_voter_get(struct device *dev, const char *name)
 	np = dev->of_node;
 
 	if (name) {
-		idx = of_property_match_string(np, "qcom,bcm-voter-names",
-						name);
+		idx = of_property_match_string(np, "qcom,bcm-voter-names", name);
 		if (idx < 0)
 			return ERR_PTR(idx);
 	}
 
 	node = of_parse_phandle(np, "qcom,bcm-voters", idx);
 
+	mutex_lock(&bcm_voter_lock);
 	list_for_each_entry(temp, &bcm_voters, voter_node) {
 		if (temp->np == node) {
 			voter = temp;
 			break;
 		}
 	}
+	mutex_unlock(&bcm_voter_lock);
 
 	return voter;
 }
-EXPORT_SYMBOL(of_bcm_voter_get);
+EXPORT_SYMBOL_GPL(of_bcm_voter_get);
 
 /**
  * qcom_icc_bcm_voter_add - queues up the bcm nodes that require updates
@@ -247,7 +244,7 @@ void qcom_icc_bcm_voter_add(struct bcm_voter *voter, struct qcom_icc_bcm *bcm)
 
 	mutex_unlock(&voter->lock);
 }
-EXPORT_SYMBOL(qcom_icc_bcm_voter_add);
+EXPORT_SYMBOL_GPL(qcom_icc_bcm_voter_add);
 
 /**
  * qcom_icc_bcm_voter_commit - generates and commits tcs cmds based on bcms
@@ -265,7 +262,7 @@ int qcom_icc_bcm_voter_commit(struct bcm_voter *voter)
 {
 	struct qcom_icc_bcm *bcm;
 	struct qcom_icc_bcm *bcm_tmp;
-	int commit_idx[MAX_VCD];
+	int commit_idx[MAX_VCD + 1];
 	struct tcs_cmd cmds[MAX_BCMS];
 	int ret = 0;
 
@@ -322,8 +319,6 @@ int qcom_icc_bcm_voter_commit(struct bcm_voter *voter)
 	list_for_each_entry_safe(bcm, bcm_tmp, &voter->commit_list, list)
 		list_del_init(&bcm->list);
 
-	INIT_LIST_HEAD(&voter->commit_list);
-
 	list_for_each_entry_safe(bcm, bcm_tmp, &voter->ws_list, ws_list) {
 		/*
 		 * Only generate WAKE and SLEEP commands if a resource's
@@ -346,8 +341,7 @@ int qcom_icc_bcm_voter_commit(struct bcm_voter *voter)
 
 	tcs_list_gen(voter, QCOM_ICC_BUCKET_WAKE, cmds, commit_idx);
 
-	ret = rpmh_write_batch(voter->dev, RPMH_WAKE_ONLY_STATE, cmds,
-				commit_idx);
+	ret = rpmh_write_batch(voter->dev, RPMH_WAKE_ONLY_STATE, cmds, commit_idx);
 	if (ret) {
 		pr_err("Error sending WAKE RPMH requests (%d)\n", ret);
 		goto out;
@@ -365,11 +359,10 @@ out:
 	list_for_each_entry_safe(bcm, bcm_tmp, &voter->commit_list, list)
 		list_del_init(&bcm->list);
 
-	INIT_LIST_HEAD(&voter->commit_list);
 	mutex_unlock(&voter->lock);
 	return ret;
 }
-EXPORT_SYMBOL(qcom_icc_bcm_voter_commit);
+EXPORT_SYMBOL_GPL(qcom_icc_bcm_voter_commit);
 
 /**
  * qcom_icc_bcm_voter_clear_init - clear init flag used during boot up
@@ -405,36 +398,28 @@ static int qcom_icc_bcm_voter_probe(struct platform_device *pdev)
 	mutex_init(&voter->lock);
 	INIT_LIST_HEAD(&voter->commit_list);
 	INIT_LIST_HEAD(&voter->ws_list);
+
+	mutex_lock(&bcm_voter_lock);
 	list_add_tail(&voter->voter_node, &bcm_voters);
+	mutex_unlock(&bcm_voter_lock);
 
 	return 0;
 }
 
 static const struct of_device_id bcm_voter_of_match[] = {
-	{ .compatible = "qcom,sdm845-bcm-voter" },
 	{ .compatible = "qcom,bcm-voter" },
-	{ },
+	{ }
 };
 
 static struct platform_driver qcom_icc_bcm_voter_driver = {
 	.probe = qcom_icc_bcm_voter_probe,
 	.driver = {
-		.name		= "sdm845_bcm_voter",
+		.name		= "bcm_voter",
 		.of_match_table = bcm_voter_of_match,
 	},
 };
+module_platform_driver(qcom_icc_bcm_voter_driver);
 
-static int __init bcm_voter_driver_init(void)
-{
-	return platform_driver_register(&qcom_icc_bcm_voter_driver);
-}
-core_initcall(bcm_voter_driver_init);
-
-static void __exit bcm_voter_driver_exit(void)
-{
-	platform_driver_unregister(&qcom_icc_bcm_voter_driver);
-}
-module_exit(bcm_voter_driver_exit);
-
-MODULE_DESCRIPTION("QTI BCM Voter interconnect driver");
+MODULE_AUTHOR("David Dai <daidavid1@codeaurora.org>");
+MODULE_DESCRIPTION("Qualcomm BCM Voter interconnect driver");
 MODULE_LICENSE("GPL v2");
