ec67ae907eaa2c7a6d75dc8cacce1c577b20319a
Divyanshu-Modi <divyan.m05@gmail.com>
2023-05-28 17:53:43 +0530

    techpack: asoc: aw88263s_m20: decrease verbosity
     dmesg is merely filled with these
    
    Change-Id: I00c05e7849c5685a337251f80c167d1c9e402d23
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- techpack/audio/asoc/codecs/aw88263s_m20/inc/aw882xx_log.h
+++ techpack/audio/asoc/codecs/aw88263s_m20/inc/aw882xx_log.h
@@ -4,6 +4,7 @@
 /********************************************
  * print information control
  *******************************************/
+#ifdef DEBUG
 #define aw_dev_err(dev, format, ...) \
 	do { \
 		pr_err("[Awinic][%s]%s: " format "\n", dev_name(dev), __func__, ##__VA_ARGS__); \
@@ -29,6 +30,13 @@
 		do { \
 			pr_debug("[Awinic]%s: " format "\n", __func__, ##__VA_ARGS__); \
 		} while (0)
-
+#else
+#define aw_dev_err(dev, format, ...) do { }while (0)
+#define aw_dev_info(dev, format, ...) do { } while (0)
+#define aw_dev_dbg(dev, format, ...) do { } while (0)
+#define aw_pr_err(format, ...) do { } while (0)
+#define aw_pr_info(format, ...) do { } while (0)
+#define aw_pr_dbg(format, ...) do { } while (0)
 #endif
 
+#endif
