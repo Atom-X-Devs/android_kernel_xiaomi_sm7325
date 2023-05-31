6633f896f3ac55fc0a79b0aa5d2a618609a7cb12
Peter Zijlstra <peterz@infradead.org>
2023-05-31 13:58:47 +0200

    sched/fair: Commit to EEVDF
    EEVDF is a better defined scheduling policy, as a result it has less
    heuristics/tunables. There is no compelling reason to keep CFS around.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20230531124604.137187212@infradead.org
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -15,13 +15,6 @@ SCHED_FEAT(PLACE_DEADLINE_INITIAL, true)
 SCHED_FEAT(NEXT_BUDDY, false)
 
 /*
- * Prefer to schedule the task that ran last (when we did
- * wake-preempt) as that likely will touch the same data, increases
- * cache locality.
- */
-SCHED_FEAT(LAST_BUDDY, true)
-
-/*
  * Consider buddies to be cache hot, decreases the likelyness of a
  * cache buddy being migrated away, increases cache locality.
  */
@@ -90,5 +83,3 @@ SCHED_FEAT(UTIL_EST, true)
  */
 SCHED_FEAT(SUGOV_RT_MAX_FREQ, false)
 SCHED_FEAT(UTIL_EST_FASTUP, true)
-
-SCHED_FEAT(EEVDF, true)
