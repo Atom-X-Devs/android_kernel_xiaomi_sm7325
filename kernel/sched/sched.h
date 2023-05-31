4dd6859f85a67ad23ae9d6059c30a4886b4ccd1c
Peter Zijlstra <peterz@infradead.org>
2023-05-31 13:58:40 +0200

    sched/fair: Add cfs_rq::avg_vruntime
    In order to move to an eligibility based scheduling policy, we need
    to have a better approximation of the ideal scheduler.
    
    Specifically, for a virtual time weighted fair queueing based
    scheduler the ideal scheduler will be the weighted average of the
    individual virtual runtimes (math in the comment).
    
    As such, compute the weighted average to approximate the ideal
    scheduler -- note that the approximation is in the individual task
    behaviour, which isn't strictly conformant.
    
    Specifically consider adding a task with a vruntime left of center, in
    this case the average will move backwards in time -- something the
    ideal scheduler would of course never do.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20230531124603.654144274@infradead.org
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -662,6 +662,9 @@ struct cfs_rq {
 	unsigned int		h_nr_running;      /* SCHED_{NORMAL,BATCH,IDLE} */
 	unsigned int		idle_h_nr_running; /* SCHED_IDLE */
 
+	s64			avg_vruntime;
+	u64			avg_load;
+
 	u64			exec_clock;
 	u64			min_vruntime;
 #ifndef CONFIG_64BIT
@@ -3411,6 +3414,8 @@ struct sched_avg_stats {
 	int nr_scaled;
 };
 extern void sched_get_nr_running_avg(struct sched_avg_stats *stats);
+
+extern u64 avg_vruntime(struct cfs_rq *cfs_rq);
 
 #ifdef CONFIG_SMP
 #ifdef CONFIG_SCHED_WALT
