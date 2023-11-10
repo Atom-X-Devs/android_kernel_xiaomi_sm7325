457e029695e7d07795fb0bea11981dabd19864a2
Lynus Vaz <quic_lvaz@quicinc.com>
2023-11-10 07:46:49 -0800

    kgsl: Cancel waittimestamp events on timeout
    When a waittimestamp times out, the corresponding event structure still
    remains on the event list. If repeated timeouts occur, this can cause them
    to pile up and delay processing of other events. Instead of leaving these
    events active, cancel them.
    
    Change-Id: I4932636a4dfbec8a01533fafe772a99b1c48167a
    Signed-off-by: Lynus Vaz <quic_lvaz@quicinc.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/gpu/msm/adreno_drawctxt.c
+++ drivers/gpu/msm/adreno_drawctxt.c
@@ -148,11 +148,11 @@ int adreno_drawctxt_wait(struct adreno_device *adreno_dev,
 			_check_context_timestamp(device, context, timestamp),
 			msecs_to_jiffies(timeout));
 
-	if (ret_temp == 0) {
-		ret = -ETIMEDOUT;
-		goto done;
-	} else if (ret_temp < 0) {
-		ret = (int) ret_temp;
+	if (ret_temp <= 0) {
+		kgsl_cancel_event(device, &context->events, timestamp,
+			wait_callback, (void *)drawctxt);
+
+		ret = ret_temp ? (int)ret_temp : -ETIMEDOUT;
 		goto done;
 	}
 	ret = 0;
