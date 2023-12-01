470c1437cb173221e31df31ca33f34ad490378a0
Thomas Gleixner <tglx@linutronix.de>
2023-12-01 10:26:33 +0100

    BACKPORT: timers: Rework idle logic
    To improve readability of the code, split base->idle calculation and
    expires calculation into separate parts. While at it, update the comment
    about timer base idle marking.
    
    Thereby the following subtle change happens if the next event is just one
    jiffy ahead and the tick was already stopped: Originally base->is_idle
    remains true in this situation. Now base->is_idle turns to false. This may
    spare an IPI if a timer is enqueued remotely to an idle CPU that is going
    to tick on the next jiffy.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Anna-Maria Behnsen <anna-maria@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Frederic Weisbecker <frederic@kernel.org>
    Link: https://lore.kernel.org/r/20231201092654.34614-12-anna-maria@linutronix.de
    [Tashar02: Backport to msm-5.4 without the timer tracing logic]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -1721,22 +1721,23 @@ u64 get_next_timer_interrupt(unsigned long basej, u64 basem)
 	 */
 	__forward_timer_base(base, basej);
 
-	if (time_before_eq(nextevt, basej)) {
-		expires = basem;
-		base->is_idle = false;
-	} else {
-		if (base->timers_pending)
-			expires = basem + (u64)(nextevt - basej) * TICK_NSEC;
-		/*
-		 * If we expect to sleep more than a tick, mark the base idle.
-		 * Also the tick is stopped so any added timer must forward
-		 * the base clk itself to keep granularity small. This idle
-		 * logic is only maintained for the BASE_STD base, deferrable
-		 * timers may still see large granularity skew (by design).
-		 */
-		if ((expires - basem) > TICK_NSEC)
-			base->is_idle = true;
+	if (base->timers_pending) {
+		/* If we missed a tick already, force 0 delta */
+		if (time_before(nextevt, basej))
+			nextevt = basej;
+		expires = basem + (u64)(nextevt - basej) * TICK_NSEC;
 	}
+
+	/*
+	 * Base is idle if the next event is more than a tick away.
+	 *
+	 * If the base is marked idle then any timer add operation must forward
+	 * the base clk itself to keep granularity small. This idle logic is
+	 * only maintained for the BASE_STD base, deferrable timers may still
+	 * see large granularity skew (by design).
+	 */
+	base->is_idle = time_after(nextevt, basej + 1);
+
 	raw_spin_unlock(&base->lock);
 
 	return cmp_next_hrtimer_event(basem, expires);
