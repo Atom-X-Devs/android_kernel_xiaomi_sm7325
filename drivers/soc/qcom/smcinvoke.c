f64df074f0a609a0ce42fd199cb11d46a037be24
Divisha Bisht <quic_divibish@quicinc.com>
2024-01-05 15:16:21 +0530

    smcinvoke: Rename size_add to smci_size_add
    Rename size_add api of smcivoke driver to smci_size_add, as
    smcinvoke's size_add is giving redefinition error with the size_add
    api recently added in upstream patch 'overflow: Implement size_t
    saturating arithmetic helpers'.
    cherry-pick hash 4dab23059f51e324451b8fe02debc3df8812afd8.
    
    Change-Id: I864c3065e63f53a3f4473edc24dc96d067dbfe60
    Signed-off-by: Divisha Bisht <quic_divibish@quicinc.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/soc/qcom/smcinvoke.c
+++ drivers/soc/qcom/smcinvoke.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2016-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022, 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #define pr_fmt(fmt) "smcinvoke: %s: " fmt, __func__
@@ -898,21 +898,30 @@ static struct smcinvoke_cb_txn *find_cbtxn_locked(
 }
 
 /*
- * pad_size is used along with size_align to define a buffer overflow
+ * smci_size_add saturates at SIZE_MAX. If integer overflow is detected,
+ * this function would return SIZE_MAX otherwise normal a+b is returned.
+ */
+static inline size_t smci_size_add(size_t a, size_t b)
+{
+	return (b > (SIZE_MAX - a)) ? SIZE_MAX : a + b;
+}
+
+/*
+ * smci_pad_size is used along with smci_size_align to define a buffer overflow
  * protected version of ALIGN
  */
-static inline size_t pad_size(size_t a, size_t b)
+static inline size_t smci_pad_size(size_t a, size_t b)
 {
 	return (~a + 1) % b;
 }
 
 /*
- * size_align saturates at SIZE_MAX. If integer overflow is detected, this
+ * smci_size_align saturates at SIZE_MAX. If integer overflow is detected, this
  * function would return SIZE_MAX otherwise next aligned size is returned.
  */
-static inline size_t size_align(size_t a, size_t b)
+static inline size_t smci_size_align(size_t a, size_t b)
 {
-	return size_add(a, pad_size(a, b));
+	return smci_size_add(a, smci_pad_size(a, b));
 }
 
 static uint16_t get_server_id(int cb_server_fd)
@@ -1711,7 +1720,7 @@ static void process_piggyback_cb_data(uint8_t *outbuf, size_t buf_len)
 		piggyback_offset = max_offset + buffer_size_max_offset;
 	else
 		piggyback_offset = TZCB_BUF_OFFSET(msg);
-	piggyback_offset = size_align(piggyback_offset, SMCINVOKE_ARGS_ALIGN_SIZE);
+	piggyback_offset = smci_size_align(piggyback_offset, SMCINVOKE_ARGS_ALIGN_SIZE);
 	// Jump to piggy back data offset
 	piggyback_buf = (uint8_t *)msg + piggyback_offset;
 	piggyback_buf_size = g_max_cb_buf_size - piggyback_offset;
@@ -1839,8 +1848,9 @@ static size_t compute_in_msg_size(const struct smcinvoke_cmd_req *req,
 
 	/* each buffer has to be 8 bytes aligned */
 	while (i < SMCI_OBJECT_COUNTS_NUM_BUFFERS(req->counts))
-		total_size = size_add(total_size,
-		size_align(args_buf[i++].b.size, SMCINVOKE_ARGS_ALIGN_SIZE));
+		total_size = smci_size_add(total_size,
+				smci_size_align(args_buf[i++].b.size,
+				SMCINVOKE_ARGS_ALIGN_SIZE));
 
 	return PAGE_ALIGN(total_size);
 }
@@ -1869,7 +1879,7 @@ static int marshal_in_invoke_req(const struct smcinvoke_cmd_req *req,
 		return 0;
 
 	FOR_ARGS(i, req->counts, BI) {
-		offset = size_align(offset, SMCINVOKE_ARGS_ALIGN_SIZE);
+		offset = smci_size_align(offset, SMCINVOKE_ARGS_ALIGN_SIZE);
 		if ((offset > buf_size) ||
 			(args_buf[i].b.size > (buf_size - offset)))
 			goto out;
@@ -1888,7 +1898,7 @@ static int marshal_in_invoke_req(const struct smcinvoke_cmd_req *req,
 		offset += args_buf[i].b.size;
 	}
 	FOR_ARGS(i, req->counts, BO) {
-		offset = size_align(offset, SMCINVOKE_ARGS_ALIGN_SIZE);
+		offset = smci_size_align(offset, SMCINVOKE_ARGS_ALIGN_SIZE);
 		if ((offset > buf_size) ||
 			(args_buf[i].b.size > (buf_size - offset)))
 			goto out;
@@ -1949,8 +1959,8 @@ static int marshal_in_tzcb_req(const struct smcinvoke_cb_txn *cb_txn,
 					user_req->op, user_req->counts);
 
 	FOR_ARGS(i, tzcb_req->hdr.counts, BI) {
-		user_req_buf_offset = size_align(user_req_buf_offset,
-					 SMCINVOKE_ARGS_ALIGN_SIZE);
+		user_req_buf_offset = smci_size_align(user_req_buf_offset,
+				SMCINVOKE_ARGS_ALIGN_SIZE);
 		tmp_arg.b.size = tz_args[i].b.size;
 		if ((tz_args[i].b.offset > tzcb_req_len) ||
 		    (tz_args[i].b.size > tzcb_req_len - tz_args[i].b.offset) ||
@@ -1975,8 +1985,8 @@ static int marshal_in_tzcb_req(const struct smcinvoke_cb_txn *cb_txn,
 		user_req_buf_offset += tmp_arg.b.size;
 	}
 	FOR_ARGS(i, tzcb_req->hdr.counts, BO) {
-		user_req_buf_offset = size_align(user_req_buf_offset,
-					SMCINVOKE_ARGS_ALIGN_SIZE);
+		user_req_buf_offset = smci_size_align(user_req_buf_offset,
+				SMCINVOKE_ARGS_ALIGN_SIZE);
 
 		tmp_arg.b.size = tz_args[i].b.size;
 		if ((user_req_buf_offset > user_req->buf_len) ||
