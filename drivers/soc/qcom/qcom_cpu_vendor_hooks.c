02293d8f827480c8a807510698b7a573b0cc562d
Mukesh Ojha <quic_mojha@quicinc.com>
2024-05-24 21:27:09 +0530

    soc: qcom: cpu_vendor_hook: Convert it to helper module
    qcom_cpu_vendor_hooks.c need not be a platform driver as
    all it does is just vendor hook registration and that
    does not be required to have Device tree entry.
    
    Converting it to a helper modules reduces overhead of adding
    device tree entry addition for multiple SoC.
    
    Change-Id: Ia3858879d4633d159ba045e646a8c52ee73e9924
    Signed-off-by: Mukesh Ojha <quic_mojha@quicinc.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/soc/qcom/qcom_cpu_vendor_hooks.c
+++ drivers/soc/qcom/qcom_cpu_vendor_hooks.c
@@ -20,56 +20,28 @@ static void timer_recalc_index(void *unused,
 	*expires -= 1;
 }
 
-static int cpu_vendor_hooks_driver_probe(struct platform_device *pdev)
+static int __init qcom_vendor_hook_driver_init(void)
 {
 	int ret;
 
 	ret = register_trace_android_vh_timer_calc_index(timer_recalc_index, NULL);
-	if (ret) {
-		dev_err(&pdev->dev, "Failed to android_vh_timer_calc_index hook\n");
-		return ret;
-	}
+	if (ret)
+		pr_err("Failed to android_vh_timer_calc_index hook\n");
 
 	return ret;
 }
 
-static int cpu_vendor_hooks_driver_remove(struct platform_device *pdev)
+static void __exit qcom_vendor_hook_driver_exit(void)
 {
 	/* Reset all initialized global variables and unregister callbacks. */
 	unregister_trace_android_vh_timer_calc_index(timer_recalc_index, NULL);
-	return 0;
 }
 
-static const struct of_device_id cpu_vendor_hooks_of_match[] = {
-	{ .compatible = "qcom,cpu-vendor-hooks" },
-	{ }
-};
-MODULE_DEVICE_TABLE(of, cpu_vendor_hooks_of_match);
-
-static struct platform_driver cpu_vendor_hooks_driver = {
-	.driver = {
-		.name = "qcom-cpu-vendor-hooks",
-		.of_match_table = cpu_vendor_hooks_of_match,
-	},
-	.probe = cpu_vendor_hooks_driver_probe,
-	.remove = cpu_vendor_hooks_driver_remove,
-};
-
-static int __init qcom_vendor_hook_driver_init(void)
-{
-	return platform_driver_register(&cpu_vendor_hooks_driver);
-}
 #if IS_MODULE(CONFIG_QCOM_CPU_VENDOR_HOOKS)
 module_init(qcom_vendor_hook_driver_init);
 #else
 pure_initcall(qcom_vendor_hook_driver_init);
 #endif
-
-static void __exit qcom_vendor_hook_driver_exit(void)
-{
-	return platform_driver_unregister(&cpu_vendor_hooks_driver);
-}
 module_exit(qcom_vendor_hook_driver_exit);
-
 MODULE_DESCRIPTION("QCOM CPU Vendor Hooks Driver");
 MODULE_LICENSE("GPL v2");
