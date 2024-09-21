83454293ca692327bf9a1e8632f6e27b4bca6eb4
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2024-09-22 02:21:56 +0600

    touch: focaltech_3680: Convert to "struct proc_ops"
    Change-Id: I610e6c87fbbfc2a75fea4ec262f1445571ed91c3
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- focaltech_3680/focaltech_ex_fun.c
+++ focaltech_3680/focaltech_ex_fun.c
@@ -334,10 +334,9 @@ proc_read_err:
 	return ret;
 }
 
-static const struct file_operations fts_proc_fops = {
-	.owner = THIS_MODULE,
-	.read = fts_debug_read,
-	.write = fts_debug_write,
+static const struct proc_ops fts_proc_fops = {
+	.proc_read = fts_debug_read,
+	.proc_write = fts_debug_write,
 };
 
 static int fts_ta_open(struct inode *inode, struct file *file)
@@ -394,10 +393,10 @@ static ssize_t fts_ta_read(struct file *filp, char __user *buff, size_t count,
 	return read_num;
 }
 
-static const struct file_operations fts_procta_fops = {
-	.open = fts_ta_open,
-	.release = fts_ta_release,
-	.read = fts_ta_read,
+static const struct proc_ops fts_procta_fops = {
+	.proc_open = fts_ta_open,
+	.proc_release = fts_ta_release,
+	.proc_read = fts_ta_read,
 };
 
 int fts_create_apk_debug_channel(struct fts_ts_data *ts_data)
@@ -465,8 +464,9 @@ static ssize_t fts_fw_version_read(struct file *filp, char __user *buf,
 		return cnt;
 }
 
-static const struct file_operations tp_fw_version_fops = {
-	.read = fts_fw_version_read,
+
+static const struct proc_ops tp_fw_version_fops = {
+	.proc_read = fts_fw_version_read,
 };
 
 static ssize_t fts_lockdown_info_read(struct file *filp, char __user *buf,
@@ -494,8 +494,8 @@ static ssize_t fts_lockdown_info_read(struct file *filp, char __user *buf,
 		return cnt;
 }
 
-static const struct file_operations tp_lockdown_info_fops = {
-	.read = fts_lockdown_info_read,
+static const struct proc_ops tp_lockdown_info_fops = {
+	.proc_read = fts_lockdown_info_read,
 };
 
 int fts_create_proc(struct fts_ts_data *ts_data)
