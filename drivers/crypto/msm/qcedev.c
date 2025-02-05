b5c11b82530bd0c096f1d1c406d2788610522fa5
Vishakha Malik <quic_vmallik@quicinc.com>
2025-02-05 15:08:12 +0530

    qcedev: Add required missing NULL check
    adding missing NULL check for qcedev_sha_req_cb
    and qcedev_cipher_req_cb causing crash while running stress
    qcedev tests in KASAN runs.
    
    Change-Id: I69ef31dada1c397fdedc2fc0374662991489bbd4
    Signed-off-by: Vishakha Malik <quic_vmallik@quicinc.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -363,6 +363,10 @@ void qcedev_sha_req_cb(void *cookie, unsigned char *digest,
 	if (!areq || !areq->cookie)
 		return;
 	handle = (struct qcedev_handle *) areq->cookie;
+
+	if (!handle || !handle->cntl)
+		return;
+
 	pdev = handle->cntl;
 	if (!pdev)
 		return;
@@ -391,6 +395,10 @@ void qcedev_cipher_req_cb(void *cookie, unsigned char *icv,
 	if (!areq || !areq->cookie)
 		return;
 	handle = (struct qcedev_handle *) areq->cookie;
+
+	if (!handle || !handle->cntl)
+		return;
+
 	podev = handle->cntl;
 	if (!podev)
 		return;
