5da13cb10a33da12229a2be0fc29cb7ae2f1406c
Divyanshu-Modi <divyan.m05@gmail.com>
2025-04-28 13:16:53 +0530

    Revert "kernel: time: Restore original wheel index calculation"
    This reverts commit 454e82ce676c1bb3f06283477d17dca6a60e6619.
    
    Change-Id: I715dd78157d77c49dde41e6e9da2f3af23173539
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -491,13 +491,6 @@ static inline void timer_set_idx(struct timer_list *timer, unsigned int idx)
  * Helper function to calculate the array index for a given expiry
  * time.
  */
-#ifndef CONFIG_SCHED_WALT
-static inline unsigned calc_index(unsigned expires, unsigned lvl)
-{
-	expires = (expires + LVL_GRAN(lvl)) >> LVL_SHIFT(lvl);
-	return LVL_OFFS(lvl) + (expires & LVL_MASK);
-}
-#else
 static inline unsigned calc_index(unsigned expires, unsigned lvl)
 {
 	if (lvl != 0 && (expires & ~(UINT_MAX << LVL_SHIFT(lvl))))
@@ -507,7 +500,6 @@ static inline unsigned calc_index(unsigned expires, unsigned lvl)
 
 	return LVL_OFFS(lvl) + (expires & LVL_MASK);
 }
-#endif
 
 static int calc_wheel_index(unsigned long expires, unsigned long clk)
 {
