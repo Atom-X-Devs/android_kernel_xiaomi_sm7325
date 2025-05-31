c59d7782ad0667e8fcfd45e25f25447c1eebe0c9
Divyanshu-Modi <divyan.m05@gmail.com>
2025-05-31 22:01:07 +0530

    sched: drop android_vh_em_pd_energy vendor hook
    Drop in favor of upstream patch
    
    Change-Id: I901d568dbb91ce6f1d783c597818b17f1a1512a5

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7022,7 +7022,6 @@ compute_energy(struct task_struct *p, int dst_cpu, struct perf_domain *pd)
 	unsigned long cpu_cap = arch_scale_cpu_capacity(cpumask_first(pd_mask));
 #endif
 	unsigned long max_util = 0, sum_util = 0;
-	unsigned long energy = 0;
 	int cpu;
 	unsigned long cpu_util;
 
@@ -7065,11 +7064,7 @@ compute_energy(struct task_struct *p, int dst_cpu, struct perf_domain *pd)
 		max_util = max(max_util, cpu_util);
 	}
 
-	trace_android_vh_em_pd_energy(pd->em_pd, max_util, sum_util, &energy);
-	if (!energy)
-		energy = em_pd_energy(pd->em_pd, max_util, sum_util);
-
-	return energy;
+	return em_pd_energy(pd->em_pd, max_util, sum_util);
 }
 
 #ifdef CONFIG_SCHED_WALT
