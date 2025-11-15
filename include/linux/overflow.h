577d2ef9c6f09da92ea97a0a65121f2b27fc82e0
Divyanshu-Modi <divyan.m05@gmail.com>
2025-11-15 21:04:34 +0530

    overflow: Fix merge derp
    The recent merge duplicated some function defines
    
    Change-Id: I79d14dc335e61311b29e3f4a4c43c50c18c0a627
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- include/linux/overflow.h
+++ include/linux/overflow.h
@@ -323,69 +323,6 @@ static inline size_t __must_check size_sub(size_t minuend, size_t subtrahend)
 }
 
 /**
- * size_mul() - Calculate size_t multiplication with saturation at SIZE_MAX
- *
- * @factor1: first factor
- * @factor2: second factor
- *
- * Returns: calculate @factor1 * @factor2, both promoted to size_t,
- * with any overflow causing the return value to be SIZE_MAX. The
- * lvalue must be size_t to avoid implicit type conversion.
- */
-static inline size_t __must_check size_mul(size_t factor1, size_t factor2)
-{
-	size_t bytes;
-
-	if (check_mul_overflow(factor1, factor2, &bytes))
-		return SIZE_MAX;
-
-	return bytes;
-}
-
-/**
- * size_add() - Calculate size_t addition with saturation at SIZE_MAX
- *
- * @addend1: first addend
- * @addend2: second addend
- *
- * Returns: calculate @addend1 + @addend2, both promoted to size_t,
- * with any overflow causing the return value to be SIZE_MAX. The
- * lvalue must be size_t to avoid implicit type conversion.
- */
-static inline size_t __must_check size_add(size_t addend1, size_t addend2)
-{
-	size_t bytes;
-
-	if (check_add_overflow(addend1, addend2, &bytes))
-		return SIZE_MAX;
-
-	return bytes;
-}
-
-/**
- * size_sub() - Calculate size_t subtraction with saturation at SIZE_MAX
- *
- * @minuend: value to subtract from
- * @subtrahend: value to subtract from @minuend
- *
- * Returns: calculate @minuend - @subtrahend, both promoted to size_t,
- * with any overflow causing the return value to be SIZE_MAX. For
- * composition with the size_add() and size_mul() helpers, neither
- * argument may be SIZE_MAX (or the result with be forced to SIZE_MAX).
- * The lvalue must be size_t to avoid implicit type conversion.
- */
-static inline size_t __must_check size_sub(size_t minuend, size_t subtrahend)
-{
-	size_t bytes;
-
-	if (minuend == SIZE_MAX || subtrahend == SIZE_MAX ||
-	    check_sub_overflow(minuend, subtrahend, &bytes))
-		return SIZE_MAX;
-
-	return bytes;
-}
-
-/**
  * array_size() - Calculate size of 2-dimensional array.
  *
  * @a: dimension one
