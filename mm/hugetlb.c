38e82792b13b686c891c031b0596efb8c9a4dbe1
Divyanshu-Modi <divyan.m05@gmail.com>
2025-11-30 00:24:01 +0530

    Revert "BACKPORT: FROMGIT: userfaultfd: hugetlbfs: only compile UFFD helpers if config enabled"
    This reverts commit 03281a10e702662e8717babed28259528dad4803.
    
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -4354,7 +4354,6 @@ out_mutex:
 	return ret;
 }
 
-#ifdef CONFIG_USERFAULTFD
 /*
  * Used by userfaultfd UFFDIO_COPY.  Based on mcopy_atomic_pte with
  * modifications for huge pages.
@@ -4495,7 +4494,6 @@ out_release_nounlock:
 	put_page(page);
 	goto out;
 }
-#endif /* CONFIG_USERFAULTFD */
 
 long follow_hugetlb_page(struct mm_struct *mm, struct vm_area_struct *vma,
 			 struct page **pages, struct vm_area_struct **vmas,
