91da22c00e3f316e3ea58e9e030557e917147a0e
Divyanshu-Modi <divyan.m05@gmail.com>
2025-11-29 12:37:44 +0530

    Revert "mm, oom: run the showmem notifier in oom"
    This reverts commit e270acd2227a982554ff43fa89b9baef76abeb8b.
    
    Change-Id: Ic2df2c9a70be31bfae902078e9d7676dba53d13a
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -42,7 +42,6 @@
 #include <linux/kthread.h>
 #include <linux/init.h>
 #include <linux/mmu_notifier.h>
-#include <linux/show_mem_notifier.h>
 #include <linux/memory_hotplug.h>
 #include <linux/cred.h>
 #include <linux/nmi.h>
@@ -478,10 +477,7 @@ static void dump_header(struct oom_control *oc, struct task_struct *p)
 		show_mem(SHOW_MEM_FILTER_NODES, oc->nodemask);
 		if (is_dump_unreclaim_slabs())
 			dump_unreclaimable_slab();
-
-		show_mem_call_notifiers();
 	}
-
 	if (sysctl_oom_dump_tasks)
 		dump_tasks(oc);
 	if (p)
