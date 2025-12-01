nekoshirro/platform_kernel_xiaomi_marble

slimbus: qcom-ngd-ctrl: Improve error handling of probe
Handle error paths of qcom_slim_ngd_ctrl_probe function.

Change-Id: Iacb4fe8917b4bc255df745e520bbd694d92991de
Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>
Signed-off-by: Siddharth Gupta <sidgup@codeaurora.org>

drivers/slimbus/qcom-ngd-ctrl.c
@@ -1857,8 +1857,11 @@ static int qcom_slim_ngd_ctrl_probe(struct platform_device *pdev)
 
 	ctrl->nb.notifier_call = qcom_slim_ngd_ssr_notify;
 	ctrl->notifier = qcom_register_ssr_notifier("lpass", &ctrl->nb);
-	if (IS_ERR(ctrl->notifier))
-		return PTR_ERR(ctrl->notifier);
+	if (IS_ERR(ctrl->notifier)) {
+		ret = PTR_ERR(ctrl->notifier);
+		dev_err(dev, "Failed to register SSR notification: %d\n", ret);
+		goto remove_ipc_sysfs;
+	}
 
 	ctrl->dev = dev;
 	ctrl->framer.rootfreq = SLIM_ROOT_FREQ >> 3;
@@ -1883,25 +1886,42 @@ static int qcom_slim_ngd_ctrl_probe(struct platform_device *pdev)
 
 	ctrl->pdr = pdr_handle_alloc(slim_pd_status, ctrl);
 	if (IS_ERR(ctrl->pdr)) {
-		dev_err(dev, "Failed to init PDR handle\n");
-		return PTR_ERR(ctrl->pdr);
+		ret = PTR_ERR(ctrl->pdr);
+		dev_err(dev, "Failed to init PDR handle: %d\n", ret);
+		goto err_out;
 	}
 
 	pds = pdr_add_lookup(ctrl->pdr, "avs/audio", "msm/adsp/audio_pd");
 	if (IS_ERR(pds) && PTR_ERR(pds) != -EALREADY) {
+		ret = PTR_ERR(pds);
 		dev_err(dev, "pdr add lookup failed: %d\n", ret);
-		return PTR_ERR(pds);
+		goto pdr_release;
 	}
 
 	platform_driver_register(&qcom_slim_ngd_driver);
 	ret = of_qcom_slim_ngd_register(dev, ctrl);
 	if (ret) {
 		SLIM_ERR(ctrl, "qcom_slim_ngd_register failed ret:%d\n", ret);
-		return ret;
+		goto pdr_release;
 	}
 
 	SLIM_INFO(ctrl, "NGD SB controller is up!\n");
 	return 0;
+
+pdr_release:
+	pdr_handle_release(ctrl->pdr);
+err_out:
+	qcom_unregister_ssr_notifier(ctrl->notifier, &ctrl->nb);
+
+remove_ipc_sysfs:
+	if (ctrl->ipc_slimbus_log)
+		ipc_log_context_destroy(ctrl->ipc_slimbus_log);
+
+	if (ctrl->sysfs_created)
+		sysfs_remove_file(&pdev->dev.kobj,
+				  &dev_attr_debug_mask.attr);
+
+	return ret;
 }
 
 static int qcom_slim_ngd_ctrl_remove(struct platform_device *pdev)

Atom-X-Devs/android_kernel_xiaomi_sm7325

Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

drivers/slimbus/qcom-ngd-ctrl.c
@@ -1858,8 +1858,11 @@ static int qcom_slim_ngd_ctrl_probe(struct platform_device *pdev)
 
 	ctrl->nb.notifier_call = qcom_slim_ngd_ssr_notify;
 	ctrl->notifier = subsys_notif_register_notifier("lpass", &ctrl->nb);
-	if (IS_ERR(ctrl->notifier))
-		return PTR_ERR(ctrl->notifier);
+	if (IS_ERR(ctrl->notifier)) {
+		ret = PTR_ERR(ctrl->notifier);
+		dev_err(dev, "Failed to register SSR notification: %d\n", ret);
+		goto remove_ipc_sysfs;
+	}
 
 	ctrl->dev = dev;
 	ctrl->framer.rootfreq = SLIM_ROOT_FREQ >> 3;
@@ -1886,25 +1889,42 @@ static int qcom_slim_ngd_ctrl_probe(struct platform_device *pdev)
 
 	ctrl->pdr = pdr_handle_alloc(slim_pd_status, ctrl);
 	if (IS_ERR(ctrl->pdr)) {
-		dev_err(dev, "Failed to init PDR handle\n");
-		return PTR_ERR(ctrl->pdr);
+		ret = PTR_ERR(ctrl->pdr);
+		dev_err(dev, "Failed to init PDR handle: %d\n", ret);
+		goto err_out;
 	}
 
 	pds = pdr_add_lookup(ctrl->pdr, "avs/audio", "msm/adsp/audio_pd");
 	if (IS_ERR(pds) && PTR_ERR(pds) != -EALREADY) {
+		ret = PTR_ERR(pds);
 		dev_err(dev, "pdr add lookup failed: %d\n", ret);
-		return PTR_ERR(pds);
+		goto pdr_release;
 	}
 
 	platform_driver_register(&qcom_slim_ngd_driver);
 	ret = of_qcom_slim_ngd_register(dev, ctrl);
 	if (ret) {
 		SLIM_ERR(ctrl, "qcom_slim_ngd_register failed ret:%d\n", ret);
-		return ret;
+		goto pdr_release;
 	}
 
 	SLIM_INFO(ctrl, "NGD SB controller is up!\n");
 	return 0;
+
+pdr_release:
+	pdr_handle_release(ctrl->pdr);
+err_out:
+	subsys_notif_unregister_notifier(ctrl->notifier, &ctrl->nb);
+
+remove_ipc_sysfs:
+	if (ctrl->ipc_slimbus_log)
+		ipc_log_context_destroy(ctrl->ipc_slimbus_log);
+
+	if (ctrl->sysfs_created)
+		sysfs_remove_file(&pdev->dev.kobj,
+				  &dev_attr_debug_mask.attr);
+
+	return ret;
 }
 
 static int qcom_slim_ngd_ctrl_remove(struct platform_device *pdev)