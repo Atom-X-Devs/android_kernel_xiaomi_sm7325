d1b5a537c3930221f013affb2cd86aaa5909a9f3
Divyanshu-Modi <divyan.m05@gmail.com>
2026-03-15 22:34:10 +0530

    soc: qcom: mem-hooks: Drop unsupported mem hooks
    msm-5.4 is not supported any other mem hooks
    
    Change-Id: Icb73b1c87b10fcd29428e3ed0bfc4f2774ca43aa
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/soc/qcom/mem-hooks.c
+++ drivers/soc/qcom/mem-hooks.c
@@ -4,30 +4,7 @@
  */
 
 #include <linux/module.h>
-#include <trace/hooks/mm.h>
 #include <trace/hooks/signal.h>
-#include <linux/printk.h>
-
-static void readahead_set(void *data, gfp_t *flag)
-{
-	if (*flag & __GFP_MOVABLE) {
-		*flag |= __GFP_CMA;
-		*flag &= ~__GFP_HIGHMEM;
-	}
-}
-
-static void gfp_zone_set(void *data, gfp_t *flag)
-{
-	if (!IS_ENABLED(CONFIG_HIGHMEM)) {
-		if ((*flag & __GFP_MOVABLE) && !(*flag & __GFP_CMA))
-			*flag &= ~__GFP_HIGHMEM;
-	}
-}
-
-static void set_swap_cache(void *data, gfp_t *flag)
-{
-	*flag |= __GFP_CMA;
-}
 
 static void reap_eligible(void *data, struct task_struct *task, bool *reap)
 {
@@ -39,24 +16,6 @@ static void reap_eligible(void *data, struct task_struct *task, bool *reap)
 static int __init init_mem_hooks(void)
 {
 	int ret;
-
-	ret = register_trace_android_rvh_set_readahead_gfp_mask(readahead_set, NULL);
-	if (ret) {
-		pr_err("Failed to register readahead_gfp_mask hooks\n");
-		return ret;
-	}
-
-	ret = register_trace_android_rvh_set_gfp_zone_flags(gfp_zone_set, NULL);
-	if (ret) {
-		pr_err("Failed to register gfp_zone_flags hooks\n");
-		return ret;
-	}
-
-	ret = register_trace_android_rvh_set_skip_swapcache_flags(set_swap_cache, NULL);
-	if (ret) {
-		pr_err("Failed to register skip_swapcache_flags hooks\n");
-		return ret;
-	}
 
 	ret = register_trace_android_vh_process_killed(reap_eligible, NULL);
 	if (ret) {
